f93a4388c50b5012ee69b9e48b222a1e8fe460ed
Lou <lou.zhang@mail.utoronto.ca>
2019-08-13 13:33:27 -0400

    added channel and user info commands

---
+++ Roki.Core/Extentions/IUserExtensions.cs
@@ -0,0 +1,22 @@
+﻿using Discord;
+using System;
+
+namespace Roki.Extentions
+{
+    public static class IUserExtensions
+    {
+        public static Uri RealAvatarUrl(this IUser usr, int size = 0)
+        {
+            var append = size <= 0
+                ? ""
+                : $"?size={size}";
+
+            return usr.AvatarId == null
+                ? null
+                : new Uri(usr.AvatarId.StartsWith("a_", StringComparison.InvariantCulture)
+                    ? $"{DiscordConfig.CDNUrl}avatars/{usr.Id}/{usr.AvatarId}.gif" + append
+                    : usr.GetAvatarUrl(ImageFormat.Auto) + append);
+        }
+
+    }
+}
--- Roki.Core/Modules/Utility/InfoCommands.cs
+++ Roki.Core/Modules/Utility/InfoCommands.cs
@@ -56,12 +56,57 @@ namespace Roki.Modules.Utility
                     .AddField(efb => efb.WithName("Members").WithValue(guild.MemberCount.ToString()).WithIsInline(true))
                     .AddField(efb => efb.WithName("Text Channels").WithValue(textChannels.ToString()).WithIsInline(true))
                     .AddField(efb => efb.WithName("Voice Channels").WithValue(voiceChannels.ToString()).WithIsInline(true))
-                    .AddField(efb => efb.WithName("Created On").WithValue($"{createdOn:MM/dd/yyyy HH:mm}").WithIsInline(true))
+                    .AddField(efb => efb.WithName("Created on").WithValue($"{createdOn:MM/dd/yyyy HH:mm}").WithIsInline(true))
                     .AddField(efb => efb.WithName("Region").WithValue(guild.VoiceRegionId.ToString()).WithIsInline(true))
                     .AddField(efb => efb.WithName("Roles").WithValue((guild.Roles.Count - 1).ToString()).WithIsInline(true))
                     .AddField(efb => efb.WithName("Features").WithValue(features).WithIsInline(true));
                 if (Uri.IsWellFormedUriString(guild.IconUrl, UriKind.Absolute))
                     embed.WithThumbnailUrl(guild.IconUrl);
+
+                await ctx.Channel.EmbedAsync(embed).ConfigureAwait(false);
+            }
+
+            [RokiCommand, Description, Usage, Aliases]
+            [RequireContext(ContextType.Guild)]
+            public async Task ChannelInfo(ITextChannel channel = null)
+            {
+                var ch = channel ?? (ITextChannel)ctx.Channel;
+                if (ch == null)
+                    return;
+
+                var createdOn = new DateTime(2015, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc).AddMilliseconds(ch.Id >> 22);
+                var userCount = (await ch.GetUsersAsync().FlattenAsync().ConfigureAwait(false)).Count();
+
+                var embed = new EmbedBuilder().WithOkColor()
+                    .WithTitle(ch.Name)
+                    .WithDescription(ch.Topic)
+                    .AddField(efb => efb.WithName("ID").WithValue(ch.Id.ToString()).WithIsInline(true))
+                    .AddField(efb => efb.WithName("Created on").WithValue($"{createdOn:MM/dd/yyyy HH:mm}").WithIsInline(true))
+                    .AddField(efb => efb.WithName("Users").WithValue(userCount.ToString()).WithIsInline(true));
+
+                await ctx.Channel.EmbedAsync(embed).ConfigureAwait(false);
+            }
+
+            [RokiCommand, Description, Usage, Aliases]
+            [RequireContext(ContextType.Guild)]
+            public async Task UserInfo(IGuildUser user = null)
+            {
+                var usr = user ?? ctx.User as IGuildUser;
+                if (user == null)
+                    return;
+
+                var embed = new EmbedBuilder().WithOkColor()
+                    .AddField(efb => efb.WithName("Name").WithValue($"**{usr.Username}**#{usr.Discriminator}").WithIsInline(true));
+                if (!string.IsNullOrWhiteSpace(usr.Nickname))
+                    embed.AddField(efb => efb.WithName("Nickname").WithValue(usr.Nickname).WithIsInline(true));
+                embed.AddField(efb => efb.WithName("ID").WithValue(usr.Id).WithIsInline(true))
+                    .AddField(efb => efb.WithName("Joined server").WithValue($"{usr.JoinedAt?.ToString("MM/dd/yyyy HH:mm") ?? "?"}").WithIsInline(true))
+                    .AddField(efb => efb.WithName("Joined Discord").WithValue($"{usr.CreatedAt:MM/dd/yyyy HH:mm}").WithIsInline(true));
+                //.AddField(efb => efb.WithName("Roles").WithValue($"**({usr.RoleIds.Count - 1})** - {string.Join("\n", usr.GetRoles().Take(10).Where(r => r.Id != r.Guild.EveryoneRole.Id).Select(r => r.Name))}").WithIsInline(true));
+
+                var avatar = usr.RealAvatarUrl();
+                if (avatar != null && avatar.IsAbsoluteUri)
+                    embed.WithThumbnailUrl(avatar.ToString());
 
                 await ctx.Channel.EmbedAsync(embed).ConfigureAwait(false);
             }
