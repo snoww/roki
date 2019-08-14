d8eb9f66bc5d93eabbdd5548ac6316dd3529f7d6
Lou <lou.zhang@mail.utoronto.ca>
2019-08-14 12:58:54 -0400

    added parser and command options for help-modules and help-command

---
+++ Roki.Core/Common/Parser.cs
@@ -0,0 +1,19 @@
+﻿using CommandLine;
+
+namespace Roki.Common
+{
+    public static class OptionParser
+    {
+        public static (T, bool) ParseFrom<T>(T options, string[] args) where T : ICommandOptions
+        {
+            using (var p = new Parser(x => x.HelpWriter = null))
+            {
+                var res = p.ParseArguments<T>(args);
+                options = res.MapResult(x => x, x => options);
+                options.NormalizeOptions();
+
+                return (options, res.Tag == ParserResultType.Parsed);
+            }
+        }
+    }
+}
