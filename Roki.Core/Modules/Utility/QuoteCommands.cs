2c0b756809ec3b48108aae964364f856df86379d
snow <lz@snow.xyz>
2019-08-19 16:15:54 -0400

    quotes: added list quotes, show quote, add quote, delete quote commands extensions: added senderror extension for error messages

--- Roki.Core/Modules/Utility/QuoteCommands.cs
+++ Roki.Core/Modules/Utility/QuoteCommands.cs
@@ -44,6 +44,83 @@ namespace Roki.Modules.Utility
                 else
                     await ctx.Channel.EmbedAsync(new EmbedBuilder().WithErrorColor().WithDescription("No quotes found on that page."));
             }
+
+            [RokiCommand, Description, Usage, Aliases]
+            [RequireContext(ContextType.Guild)]
+            public async Task ShowQuote([Leftover] string keyword)
+            {
+                if (string.IsNullOrWhiteSpace(keyword))
+                    return;
+
+                keyword = keyword.ToUpperInvariant();
+
+                Quote quote;
+                using (var uow = _db.GetDbContext())
+                {
+                    quote = await uow.Quotes.GetRandomQuoteByKeywordAsync(ctx.Guild.Id, keyword);
+                }
+                
+                if (quote == null)
+                    return;
+                // TODO make quotes look better
+                await ctx.Channel.SendMessageAsync($"`#{quote.Id}` 📣 " + quote.Text).ConfigureAwait(false);
+            }
+
+            [RokiCommand, Description, Usage, Aliases]
+            [RequireContext(ContextType.Guild)]
+            public async Task AddQuote(string keyword, [Leftover] string text)
+            {
+                if (string.IsNullOrWhiteSpace(keyword) || string.IsNullOrWhiteSpace(text))
+                    return;
+
+                keyword = keyword.ToUpperInvariant();
+
+                using (var uow = _db.GetDbContext())
+                {
+                    uow.Quotes.Add(new Quote
+                    {
+                        AuthorId = ctx.Message.Author.Id,
+                        AuthorName = ctx.Message.Author.Username,
+                        GuildId = ctx.Guild.Id,
+                        Keyword = keyword,
+                        Text = text,
+                    });
+                    await uow.SaveChangesAsync();
+                }
+
+                await ctx.Channel.SendMessageAsync("Quote added.");
+            }
+
+            [RokiCommand, Description, Usage, Aliases]
+            [RequireContext(ContextType.Guild)]
+            public async Task QuoteDelete(int id)
+            {
+                var isAdmin = ((IGuildUser) ctx.Message.Author).GuildPermissions.Administrator;
+                var success = false;
+                string response;
+
+                using (var uow = _db.GetDbContext())
+                {
+                    var q = uow.Quotes.GetById(id);
+
+                    if (q?.GuildId != ctx.Guild.Id || !isAdmin && q.AuthorId != ctx.Message.Author.Id)
+                    {
+                        response = "No quotes found which you can remove.";
+                    }
+                    else
+                    {
+                        uow.Quotes.Remove(q);
+                        await uow.SaveChangesAsync();
+                        success = true;
+                        response = $"Quote #{id} deleted";
+                    }
+                }
+
+                if (success)
+                    await ctx.Channel.SendConfirmAsync(response).ConfigureAwait(false);
+                await ctx.Channel.SendErrorAsync(response).ConfigureAwait(false);
+            }
+            
         }
     }
     
