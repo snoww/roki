204df47b4becc854a625dae7e06ff45ccd9714fc
Lou <lou.zhang@mail.utoronto.ca>
2019-08-20 16:02:11 -0400

    calc: added textoimage command

---
+++ Roki.Core/Modules/Utility/CalcCommand.cs
@@ -0,0 +1,26 @@
+using System.Net;
+using System.Threading.Tasks;
+using Discord.Commands;
+using Roki.Common.Attributes;
+
+namespace Roki.Modules.Utility
+{
+    public partial class Utility
+    {
+        [Group]
+        public class CalcCommands : RokiSubmodule
+        {
+            public async Task TecxToImage([Leftover] string tex)
+            {
+                if (string.IsNullOrWhiteSpace(tex))
+                    return;
+                var encode = tex.Trim();
+                tex = WebUtility.UrlEncode(encode);
+
+                var result = $"https://chart.googleapis.com/chart?cht=tx&chl={tex}";
+
+                await ctx.Channel.SendMessageAsync(result).ConfigureAwait(false);
+            }
+        }
+    }
+}
