e58db1091c3f3957a6f0d142edba2e25b611d8fd
snow <lz@snow.xyz>
2019-08-21 16:07:54 -0400

    pokemon: added basic evolution chain to embed

--- Roki.Core/Modules/Searches/PokemonCommands.cs
+++ Roki.Core/Modules/Searches/PokemonCommands.cs
@@ -1,5 +1,3 @@
-using System;
-using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Discord;
@@ -27,8 +25,13 @@ namespace Roki.Modules.Searches
 
                 var pokemon = await _pokeClient.GetResourceAsync<Pokemon>(query).ConfigureAwait(false);
                 if (pokemon == null)
+                {
+                    await ctx.Channel.SendErrorAsync("No Pokémon of that name found.");
                     return;
+                }
+                    
                 var species = await _pokeClient.GetResourceAsync(pokemon.Species).ConfigureAwait(false);
+                var evoChain = await _pokeClient.GetResourceAsync(species.EvolutionChain).ConfigureAwait(false);
 
                 var embed = new EmbedBuilder().WithOkColor()
                     .WithTitle($"#{pokemon.Id} {pokemon.Name.ToTitleCase()}")
@@ -36,7 +39,23 @@ namespace Roki.Modules.Searches
                     .WithThumbnailUrl(pokemon.Sprites.FrontDefault)
                     .AddField("Types", string.Join("\n", pokemon.Types.OrderBy(t => t.Slot).Select(t => t.Type.Name.ToTitleCase()).ToList()), true)
                     .AddField("Abilities", string.Join("\n", pokemon.Abilities.OrderBy(a => a.Slot).Select(a => a.Ability.Name.ToTitleCase().Replace('-', ' ')).ToList()), true);
-                
+
+                if (evoChain != null)
+                {
+                    var evostr = "";
+                    evostr += evoChain.Chain.Species.Name + "\n";
+                    foreach (var evo in evoChain.Chain.EvolvesTo)
+                    {
+                        evostr += evo.Species.Name.ToTitleCase() + "\n";
+                        if (evo.EvolvesTo != null)
+                        {
+                            evostr += evo.EvolvesTo.Select(e => e.Species.Name) + "\n";
+                        }
+                    }
+
+                    embed.AddField("Evolution", evostr, true);
+                }
+
                 await ctx.Channel.EmbedAsync(embed).ConfigureAwait(false);
             }
         }
