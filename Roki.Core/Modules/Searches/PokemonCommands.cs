5990147f586af70f20ba460647ff7fc879ccb07b
snow <lz@snow.xyz>
2019-08-21 20:25:11 -0400

    pokemon: wrapped pokemon command in try block, started on ability command

--- Roki.Core/Modules/Searches/PokemonCommands.cs
+++ Roki.Core/Modules/Searches/PokemonCommands.cs
@@ -1,4 +1,6 @@
+using System;
 using System.Linq;
+using System.Net.Http;
 using System.Threading.Tasks;
 using Discord;
 using Discord.Commands;
@@ -25,41 +27,54 @@ namespace Roki.Modules.Searches
 
                 await ctx.Channel.TriggerTypingAsync().ConfigureAwait(false);
 
-                var pokemon = await _pokeClient.GetResourceAsync<Pokemon>(query).ConfigureAwait(false);
-                if (pokemon == null)
+                try
+                {
+                    var pokemon = await _pokeClient.GetResourceAsync<Pokemon>(query).ConfigureAwait(false);
+                    var species = await _pokeClient.GetResourceAsync(pokemon.Species).ConfigureAwait(false);
+                    var evoChain = await _pokeClient.GetResourceAsync(species.EvolutionChain).ConfigureAwait(false);
+
+                    var embed = new EmbedBuilder().WithOkColor()
+                        .WithTitle($"#{pokemon.Id} {pokemon.Name.ToTitleCase()}")
+                        .WithDescription($"{species.Genera[2].Genus}")
+                        .WithThumbnailUrl(pokemon.Sprites.FrontDefault)
+                        .AddField("Types", string.Join("\n", pokemon.Types.OrderBy(t => t.Slot).Select(t => t.Type.Name.ToTitleCase()).ToList()), true)
+                        .AddField("Abilities", string.Join("\n", pokemon.Abilities.OrderBy(a => a.Slot).Select(a => a.Ability.Name.ToTitleCase().Replace('-', ' ')).ToList()), true)
+                        .AddField("Base Stats", $"`HP: {pokemon.Stats[5].BaseStat}\nAttack: {pokemon.Stats[4].BaseStat}\nSp. Atk: {pokemon.Stats[2].BaseStat}\nDefense: {pokemon.Stats[3].BaseStat}\nSp. Def: {pokemon.Stats[1].BaseStat}\nSpeed: {pokemon.Stats[0].BaseStat}\n`", true);
+
+                    if (evoChain.Chain.EvolvesTo.Count > 0)
+                    {
+                        var evostr = "";
+                        evostr += evoChain.Chain.Species.Name.ToTitleCase() + "\n";
+                        foreach (var evo in evoChain.Chain.EvolvesTo)
+                        {
+                            evostr += evo.Species.Name.ToTitleCase() + "\n";
+                            if (evo.EvolvesTo.Count > 0)
+                            {
+                                evostr += string.Join("\n", evo.EvolvesTo.Select(e => e.Species.Name.ToTitleCase()).ToList());
+                            }
+                        }
+
+                        embed.AddField("Evolution", evostr, true);
+                    }
+                    await ctx.Channel.EmbedAsync(embed).ConfigureAwait(false);
+                }
+                catch
                 {
                     await ctx.Channel.SendErrorAsync("No Pokémon of that name found.");
+                }
+            }
+
+            [RokiCommand, Usage, Description, Aliases]
+            public async Task Ability([Leftover] string query)
+            {
+                if (string.IsNullOrWhiteSpace(query))
                     return;
-                }
-                    
-                var species = await _pokeClient.GetResourceAsync(pokemon.Species).ConfigureAwait(false);
-                var evoChain = await _pokeClient.GetResourceAsync(species.EvolutionChain).ConfigureAwait(false);
+                await ctx.Channel.TriggerTypingAsync().ConfigureAwait(false);
+                
+//                if 
 
-                var embed = new EmbedBuilder().WithOkColor()
-                    .WithTitle($"#{pokemon.Id} {pokemon.Name.ToTitleCase()}")
-                    .WithDescription($"{species.Genera[2].Genus}")
-                    .WithThumbnailUrl(pokemon.Sprites.FrontDefault)
-                    .AddField("Types", string.Join("\n", pokemon.Types.OrderBy(t => t.Slot).Select(t => t.Type.Name.ToTitleCase()).ToList()), true)
-                    .AddField("Abilities", string.Join("\n", pokemon.Abilities.OrderBy(a => a.Slot).Select(a => a.Ability.Name.ToTitleCase().Replace('-', ' ')).ToList()), true)
-                    .AddField("Base Stats", $"`HP: {pokemon.Stats[5].BaseStat}\nAttack: {pokemon.Stats[4].BaseStat}\nSp. Atk: {pokemon.Stats[2].BaseStat}\nDefense: {pokemon.Stats[3].BaseStat}\nSp. Def: {pokemon.Stats[1].BaseStat}\nSpeed: {pokemon.Stats[0].BaseStat}\n`", true);
-
-                if (evoChain.Chain.EvolvesTo.Count > 0)
-                {
-                    var evostr = "";
-                    evostr += evoChain.Chain.Species.Name.ToTitleCase() + "\n";
-                    foreach (var evo in evoChain.Chain.EvolvesTo)
-                    {
-                        evostr += evo.Species.Name.ToTitleCase() + "\n";
-                        if (evo.EvolvesTo.Count > 0)
-                        {
-                            evostr += string.Join("\n", evo.EvolvesTo.Select(e => e.Species.Name.ToTitleCase()).ToList());
-                        }
-                    }
-
-                    embed.AddField("Evolution", evostr, true);
-                }
-
-                await ctx.Channel.EmbedAsync(embed).ConfigureAwait(false);
+                var ability = await _pokeClient.GetResourceAsync<Ability>(query).ConfigureAwait(false);
+                
             }
         }
     }
