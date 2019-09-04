6c952870da1f1df01095bfbb60a215cd49d54701
snow <lz@snow.xyz>
2019-09-04 15:57:56 -0400

    twitter: added searchtweet command, sends results in a page

--- Roki.Core/Modules/Searches/TwitterCommands.cs
+++ Roki.Core/Modules/Searches/TwitterCommands.cs
@@ -1,3 +1,5 @@
+using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using Discord;
@@ -31,7 +33,7 @@ namespace Roki.Modules.Searches
             }
             
             [RokiCommand, Usage, Description, Aliases]
-            public async Task Twitter([Leftover] string query)
+            public async Task Twitter([Leftover] string query = null)
             {
                 if (string.IsNullOrWhiteSpace(query))
                 {
@@ -42,12 +44,12 @@ namespace Roki.Modules.Searches
                 const int maxTweetsToReturn = 5;
 
                 var tweets = await
-                    (from tweet in _twitterCtx.Status
-                        where tweet.Type == StatusType.User &&
-                              tweet.ScreenName == query &&
-                              tweet.Count == maxTweetsToReturn &&
-                              tweet.TweetMode == TweetMode.Extended
-                        select tweet)
+                    (from search in _twitterCtx.Status
+                        where search.Type == StatusType.User &&
+                              search.ScreenName == query &&
+                              search.Count == maxTweetsToReturn &&
+                              search.TweetMode == TweetMode.Extended
+                        select search)
                     .ToListAsync().ConfigureAwait(false);
 
                 if (tweets == null)
@@ -56,14 +58,78 @@ namespace Roki.Modules.Searches
                     return;
                 }
 
-                var latest = tweets[0];
+                var tweet = tweets[0];
                 var embed = new EmbedBuilder().WithOkColor()
-                    .WithAuthor($"{latest.User.Name}", latest.User.ProfileImageUrl)
-                    .WithDescription(latest.FullText)
-                    .AddField("Retweets", latest.RetweetCount, true)
-                    .AddField("Likes", latest.FavoriteCount ?? 0, true);
+                    .WithAuthor($"{tweet.User.ScreenNameResponse} (@{tweet.User.UserIDResponse})", tweet.User.ProfileImageUrl)
+                    .WithDescription(tweet.Text ?? tweet.FullText)
+                    .AddField("Retweets", tweet.RetweetCount, true)
+                    .AddField("Likes", tweet.FavoriteCount ?? 0, true);
                 
                 await ctx.Channel.EmbedAsync(embed);
+            }
+            [RokiCommand, Usage, Description, Aliases]
+            public async Task TwitterSearch([Leftover] string query = null)
+            {
+                if (string.IsNullOrWhiteSpace(query))
+                {
+                    await ctx.Channel.SendErrorAsync("Please provide a search query.").ConfigureAwait(false);
+                    return;
+                }
+                
+                const int maxTweetsToReturn = 5;
+                const int maxTotalResults = 10;
+                ulong sinceID = 1;
+                ulong maxId;
+
+                var combinedSearch = new List<Status>();
+                
+                var searchResponse = await
+                    (from search in _twitterCtx.Search
+                        where search.Type == SearchType.Search &&
+                              search.Query == query &&
+                              search.Count == maxTweetsToReturn &&
+                              search.SinceID == sinceID &&
+                              search.TweetMode == TweetMode.Extended
+                        select search.Statuses)
+                    .SingleOrDefaultAsync().ConfigureAwait(false);
+
+                if (searchResponse != null)
+                {
+                    combinedSearch.AddRange(searchResponse);
+                    var prevMaxId = ulong.MaxValue;
+                    do
+                    {
+                        maxId = searchResponse.Min(status => status.StatusID) - 1;
+                        Debug.Assert(maxId < prevMaxId);
+                        prevMaxId = maxId;
+
+                        searchResponse = await
+                            (from search in _twitterCtx.Search
+                                where search.Type == SearchType.Search &&
+                                      search.Query == query &&
+                                      search.Count == maxTweetsToReturn &&
+                                      search.MaxID == maxId &&
+                                      search.SinceID == sinceID &&
+                                      search.TweetMode == TweetMode.Extended
+                                select search.Statuses)
+                            .SingleOrDefaultAsync().ConfigureAwait(false);
+
+                        await ctx.SendPaginatedConfirmAsync(0, p =>
+                        {
+                            var tweet = combinedSearch[p];
+                            return new EmbedBuilder().WithOkColor()
+                                .WithAuthor($"{tweet.User.ScreenNameResponse} (@{tweet.User.UserIDResponse})", tweet.User.ProfileImageUrl)
+                                .WithDescription(tweet.Text ?? tweet.FullText)
+                                .AddField("Retweets", tweet.RetweetCount, true)
+                                .AddField("Likes", tweet.FavoriteCount ?? 0, true);
+                        }, combinedSearch.Count, 1).ConfigureAwait(false);
+
+                    } while (searchResponse.Any() && combinedSearch.Count < maxTotalResults);
+                }
+                else
+                {
+                    await ctx.Channel.SendErrorAsync("No results found.");
+                }
             }
         }
     }
