8d879ea254636c0a7ff7b469a16a85ccdce633a7
Lou <lou.zhang@mail.utoronto.ca>
2019-09-12 11:47:00 -0400

    anime: added anime search

--- Roki.Core/Modules/Searches/AnimeCommands.cs
+++ Roki.Core/Modules/Searches/AnimeCommands.cs
@@ -1,10 +1,85 @@
+using System;
+using System.Threading.Tasks;
+using Discord;
+using Discord.Commands;
+using Roki.Common.Attributes;
+using Roki.Extensions;
+using Roki.Modules.Searches.Services;
+
 namespace Roki.Modules.Searches
 {
     public partial class Searches
     {
-        public class AnimeCommands : RokiSubmodule
+        [Group]
+        public class AnimeCommands : RokiSubmodule<AnimeService>
         {
+            public async Task Anime([Leftover] string query)
+            {
+                if (string.IsNullOrWhiteSpace(query))
+                {
+                    await ctx.Channel.SendErrorAsync("No query provided.").ConfigureAwait(false);
+                    return;
+                }
+
+                var media = await _service.GetAnimeDataAsync(query).ConfigureAwait(false);
+                if (media == null)
+                {
+                    await ctx.Channel.SendErrorAsync("Couldn't find that anime :(").ConfigureAwait(false);
+                    return;
+                }
+
+                EmbedBuilder AnimeList(int curPage)
+                {
+                    var a = new EmbedBuilder().WithImageUrl("asd");
+                    var item = media[curPage];
+                    var embed = new EmbedBuilder().WithColor(Convert.ToUInt32(item.coverImage.color.ToString().Substring(1), 16))
+                        .WithTitle((item.title.english + " | " + item.title.romaji + " | " + item.title.native).ToString().TrimTo(256))
+                        .WithDescription(item.description.ToString().TrimTo(2048))
+                        .WithImageUrl(item.coverImage.large)
+                        .AddField("Type", item.type, true)
+                        .AddField("Genres", string.Join(", ", item.genres), true)
+                        .AddField("Status", item.status, true)
+                        .AddField("Episodes", item.episodes, true)
+                        .AddField("Release Year", item.seasonInt.GetReleaseYear(), true)
+                        .AddField("Rating", $"{item.averageScore}/100");
+                    return embed;
+                }
+
+                await ctx.SendPaginatedConfirmAsync((int) 0, (Func<int, EmbedBuilder>) AnimeList, (int) media.Count, (int) 1, (bool) false).ConfigureAwait(false);
+            }
+        }
+    }
+    public static class AnimeExtensions
+    {
+        public static string GetReleaseYear(int seasonInt)
+        {
+            var year = seasonInt.ToString().Substring(0, seasonInt.ToString().Length - 1);
+            var season = seasonInt.ToString().Substring(seasonInt.ToString().Length - 1);
             
+            if (string.IsNullOrWhiteSpace(year))
+                year = "2000";
+            else if (int.Parse(year) > 0 && int.Parse(year) < 10)
+                year = "200" + year;
+            else if (int.Parse(year) > 30)
+                year = "19" + year;
+            
+            switch (season)
+            {
+                case "1":
+                    season = "Spring";
+                    break;
+                case "2":
+                    season = "Summer";
+                    break;
+                case "3":
+                    season = "Fall";
+                    break;
+                case "4":
+                    season = "Winter";
+                    break;
+            }
+
+            return season + " " + year;
         }
     }
 }
--- Roki.Core/Modules/Searches/Services/AnimeService.cs
+++ Roki.Core/Modules/Searches/Services/AnimeService.cs
@@ -1,16 +1,19 @@
+using System;
 using System.Collections.Generic;
 using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 using Roki.Core.Services;
+using Roki.Extensions;
 
 namespace Roki.Modules.Searches.Services
 {
     public class AnimeService : IRService
     {
         private string queryJson =
-            @"{""query"":""query ($search: String) { Page(page:1 perPage:5) { media(search: $search) { title { romaji english native } description averageScore status episodes genres type seasonInt coverImage { large color } } } }"", ""variables"": { ""search"": searchstring } }";
+            @"{""query"":""query ($search: String) { Page(page:1 perPage:5) { media(search: $search) { title { romaji english native } description averageScore status episodes genres type seasonInt coverImage { large color } } } }"", ""variables"": { ""search"": searchString } }";
         private readonly IHttpClientFactory _httpFactory;
 
         public AnimeService(IHttpClientFactory httpFactory)
@@ -18,56 +21,27 @@ namespace Roki.Modules.Searches.Services
             _httpFactory = httpFactory;
         }
 
-        public async Task<AnimeData> GetAnimeDataAsync(string query)
+        public async Task<dynamic> GetAnimeDataAsync(string query)
         {
-            using (var http = _httpFactory.CreateClient())
+            try
             {
-                var content = new StringContent(queryJson, Encoding.UTF8, "application/json");
-                var result = await http.PostAsync("https://graphql.anilist.co", content).ConfigureAwait(false);
-                var data = JsonConvert.DeserializeObject<AnimeData>(await result.Content.ReadAsStringAsync().ConfigureAwait(false));
+                query = query.SanitizeString();
+                using (var http = _httpFactory.CreateClient())
+                {
+                    queryJson = queryJson.Replace("searchString", query);
+                    var content = new StringContent(queryJson, Encoding.UTF8, "application/json");
+                    var result = await http.PostAsync("https://graphql.anilist.co", content).ConfigureAwait(false);
+                    dynamic json = JsonConvert.DeserializeObject(await result.Content.ReadAsStringAsync().ConfigureAwait(false));
+                    var media = json.data["Page"].media;
+                    return media;
+                }
             }
+            catch
+            {
+                return null;
+            }
+            
         }
         
-    }
-
-    public class AnimeData
-    {
-        public Dictionary<string, AnimePage> Data { get; set; }
-    }
-    
-    public class AnimePage
-    {
-        public Dictionary<string, AnimeMedia> Page { get; set; }
-    }
-    
-    public class AnimeMedia
-    {
-        public Dictionary<string, AnimeModel> Media { get; set; }
-    }
-
-    public class AnimeModel
-    {
-        public AnimeTitle Title { get; set; }
-        public string Description { get; set; }
-        public int AverageScore { get; set; }
-        public string Status { get; set; }
-        public int Episodes { get; set; }
-        public string[] Genres { get; set; }
-        public string Type { get; set; }
-        public int SeasonInt { get; set; }
-        public CoverImage CoverImage { get; set; }
-    }
-
-    public class AnimeTitle
-    {
-        public string Romaji { get; set; }
-        public string English { get; set; }
-        public string Native { get; set; }
-    }
-
-    public class CoverImage
-    {
-        public string Large { get; set; }
-        public string Color { get; set; }
     }
 }
