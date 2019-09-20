116b4ff1d0de788cee966b62709df69b9e42106f
Lou <lou.zhang@mail.utoronto.ca>
2019-09-20 11:14:27 -0400

    db: setup entityframework for mysql

--- Roki.Core/Services/Database/RokiContext.cs
+++ Roki.Core/Services/Database/RokiContext.cs
@@ -1,42 +1,48 @@
 using System.IO;
-using Microsoft.Data.Sqlite;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore.Design;
+using MySql.Data.EntityFrameworkCore;
 using Roki.Core.Services.Database.Models;
 using Roki.Core.Services.Impl;
 
 namespace Roki.Core.Services.Database
 {
-    public class RokiContextFactory : IDesignTimeDbContextFactory<RokiContext>
-    {
-        public RokiContext CreateDbContext(string[] args)
-        {
-            var optionsBuilder = new DbContextOptionsBuilder<RokiContext>();
-            IRokiConfig config = new RokiConfig();
-            var builder = new SqliteConnectionStringBuilder(config.Db.ConnectionString);
-            builder.DataSource = Path.Combine(Directory.GetCurrentDirectory(), builder.DataSource);
-            optionsBuilder.UseSqlite(builder.ToString());
-            var ctx = new RokiContext(optionsBuilder.Options);
-            ctx.Database.SetCommandTimeout(60);
-            return ctx;
-        }
-    }
+//    public class RokiContextFactory : IDesignTimeDbContextFactory<RokiContext>
+//    {
+//        public RokiContext CreateDbContext(string[] args)
+//        {
+//            var optionsBuilder = new DbContextOptionsBuilder<RokiContext>();
+//            IRokiConfig config = new RokiConfig();
+//            var builder = new SqliteConnectionStringBuilder(config.Db.ConnectionString);
+//            builder.DataSource = Path.Combine(Directory.GetCurrentDirectory(), builder.DataSource);
+//            optionsBuilder.UseSqlite(builder.ToString());
+//            var ctx = new RokiContext(optionsBuilder.Options);
+//            ctx.Database.SetCommandTimeout(60);
+//            return ctx;
+//        }
+//    }
 
     public class RokiContext : DbContext
     {
-        public RokiContext(DbContextOptions<RokiContext> options) : base(options)
+        public RokiContext(DbContextOptions options) : base(options)
         {
         }
 
         public DbSet<Quote> Quotes { get; set; }
 
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseMySQL("server=localhost;database=roki;user=roki;password=roki");
+        }
+
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             #region QUOTES
 
-            var quoteEntity = modelBuilder.Entity<Quote>();
-            quoteEntity.HasIndex(x => x.GuildId);
-            quoteEntity.HasIndex(x => x.Keyword);
+            modelBuilder.Entity<Quote>(entity =>
+            {
+                entity.HasIndex(x => x.GuildId);
+                entity.HasIndex(x => x.Keyword);
+            });
 
             #endregion
         }
