ab3721b48d4a909a9029b2e6924a67ef63d5c77e
Lou <lou.zhang@mail.utoronto.ca>
2019-09-23 15:14:07 -0400

    xp: added basic xp command to show xp for testing

---
+++ Roki.Core/Modules/Xp/Xp.cs
@@ -0,0 +1,31 @@
+using System.Threading.Tasks;
+using Discord;
+using Roki.Common.Attributes;
+using Roki.Core.Services;
+using Roki.Modules.Xp.Common;
+
+namespace Roki.Modules.Xp
+{
+    public partial class Xp : RokiTopLevelModule
+    {
+        private readonly DbService _db;
+
+
+        public Xp(DbService db)
+        {
+            _db = db;
+        }
+        
+        [RokiCommand, Description, Usage, Aliases]
+        public async Task Experience([Leftover] IUser user = null)
+        {
+            user = user ?? ctx.User;
+            using (var uow = _db.GetDbContext())
+            {
+                var dUser = uow.DUsers.GetOrCreate(user);
+                var xp = new XpLevel(dUser.TotalXp);
+                await ctx.Channel.SendMessageAsync($"{user.Username}\nLevel: {xp.Level}\nTotal XP: {xp.TotalXp}\nXP to next Level: {xp.RequiredXp}");
+            }
+        }
+    }
+}
