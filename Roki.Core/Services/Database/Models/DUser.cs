83c2c265658bfa7c2a1b1a3dc36c9641ccf75de1
Lou <lou.zhang@mail.utoronto.ca>
2019-09-24 09:57:04 -0400

    xp: forgot to change table name for duser

--- Roki.Core/Services/Database/Models/DUser.cs
+++ Roki.Core/Services/Database/Models/DUser.cs
@@ -3,7 +3,7 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Roki.Core.Services.Database.Models
 {
-    [Table("userstest")]
+    [Table("users")]
     public class DUser : DbEntity
     {
         public ulong UserId { get; set; }
