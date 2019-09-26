30a3597a3b43b7aee0a1d68d61f63f163ea69900
Lou <lou.zhang@mail.utoronto.ca>
2019-09-26 12:04:04 -0400

    currency: added currency generation service, along with more properties for transactions log

--- Roki.Core/Services/MessageService.cs
+++ Roki.Core/Services/MessageService.cs
@@ -1,8 +1,10 @@
 using System;
+using System.Collections.Concurrent;
 using System.Threading.Tasks;
 using Discord;
 using Discord.WebSocket;
 using Roki.Core.Services;
+using Roki.Core.Services.Database;
 using Roki.Core.Services.Database.Models;
 
 namespace Roki.Services
@@ -11,11 +13,14 @@ namespace Roki.Services
     {
         private readonly DiscordSocketClient _client;
         private readonly DbService _db;
+        private readonly CommandHandler _cmdHandler;
+        public ConcurrentDictionary<ulong, DateTime> LastGenerations { get; } = new ConcurrentDictionary<ulong, DateTime>();
 
-        public MessageService(DiscordSocketClient client, DbService db)
+        public MessageService(DiscordSocketClient client, DbService db, CommandHandler cmdHandler)
         {
             _client = client;
             _db = db;
+            _cmdHandler = cmdHandler;
         }
 
         public async Task StartService()
@@ -26,6 +31,13 @@ namespace Roki.Services
                 {
                     if (!message.Author.IsBot)
                     {
+                        await CurrencyGeneration(message).ConfigureAwait(false);
+                        
+                        var user = uow.DUsers.GetOrCreate(message.Author);
+                        
+                        if (DateTime.UtcNow - user.LastXpGain >= TimeSpan.FromMinutes(5))
+                            await uow.DUsers.UpdateXp(user, message).ConfigureAwait(false);
+
                         uow.DMessages.Add(new DMessage
                         {
                             AuthorId = message.Author.Id,
@@ -40,12 +52,6 @@ namespace Roki.Services
                             Timestamp = message.Timestamp.UtcDateTime
                         });
 
-                        var user = uow.DUsers.GetOrCreate(message.Author);
-                        if (DateTime.UtcNow - user.LastXpGain >= TimeSpan.FromMinutes(5))
-                        {
-                            await uow.DUsers.UpdateXp(user, message).ConfigureAwait(false);
-                        }
-                        
                         await uow.SaveChangesAsync().ConfigureAwait(false);
                     }
                 }
@@ -94,6 +100,41 @@ namespace Roki.Services
             };
             
             await Task.CompletedTask;
+        }
+
+        private async Task CurrencyGeneration(SocketMessage message)
+        {
+            if (!(message is SocketUserMessage msg))
+                return;
+            if (!(message.Channel is ITextChannel channel))
+                return;
+            // TODO add ignored channels, change constants to database values
+
+            var lastGeneration = LastGenerations.GetOrAdd(channel.Id, DateTime.MinValue);
+            var rng = new Random();
+            
+            if (DateTime.UtcNow - TimeSpan.FromMinutes(5) < lastGeneration)
+                return;
+
+            var num = rng.Next(1, 101) + 1.5 * 100;
+            if (num > 100 && LastGenerations.TryUpdate(channel.Id, DateTime.UtcNow, lastGeneration))
+            {
+                var drop = 1;
+                var dropMax = 5;
+                
+                if (dropMax != null && dropMax > drop)
+                    drop = new Random().Next(drop, dropMax + 1);
+
+                if (drop > 0)
+                {
+                    var prefix = _cmdHandler.DefaultPrefix;
+                    var toSend = drop == 1
+                        ? $"A random stone appeared! Type `{prefix}pick` to pick it up."
+                        : $"Some random stones appeared! Type `{prefix}pick` to pick them up.";
+                    // TODO add images to send with drop
+                    await channel.SendMessageAsync(toSend).ConfigureAwait(false);
+                }
+            }
         }
     }
 }
