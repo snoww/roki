0eb6749069c93a1a26a2e819fb9cdccf76140481
Lou <lou.zhang@mail.utoronto.ca>
2019-09-27 11:38:17 -0400

    currency: updated syntax in sql query

--- Roki.Core/Services/Database/Repositories/CurrencyTransactionRepository.cs
+++ Roki.Core/Services/Database/Repositories/CurrencyTransactionRepository.cs
@@ -24,9 +24,9 @@ namespace Roki.Core.Services.Database.Repositories
             var toReturn = (trans.Sum(t => t.Amount), trans.Select(t => t.MessageId).ToArray());
             await Context.Database.ExecuteSqlCommandAsync($@"
 UPDATE IGNORE transactions
-SET Reason={"GCPicked"}
-    To={userId}
-WHERE ChannelId={channelId} AND Reason={"GCA"}
+SET `Reason`={"GCPicked"},
+    `To`={userId}
+WHERE `ChannelId`={channelId} AND `Reason`={"GCA"}
 ").ConfigureAwait(false);
             return toReturn;
         }
