761b5de363354bccd3b4f0586a0ccb2b19b24403
Lou <lou.zhang@mail.utoronto.ca>
2019-10-03 14:56:03 -0400

    gambling: added betflip command

--- Roki.Core/Services/CurrencyService.cs
+++ Roki.Core/Services/CurrencyService.cs
@@ -10,9 +10,9 @@ namespace Roki.Services
 {
     public interface ICurrencyService : IRService
     {
-        Task ChangeAsync(IUser user, string reason, long amount, string from, ulong guildId, ulong channelId, ulong messageId);
-        Task ChangeListAsync(IEnumerable<IUser> users, IEnumerable<string> reasons, IEnumerable<long> amounts, IEnumerable<string> from,
-            IEnumerable<ulong> guildIds, IEnumerable<ulong> channelIds, IEnumerable<ulong> messageIds);
+        Task<bool> ChangeAsync(IUser user, string reason, long amount, string from, string to, ulong guildId, ulong channelId, ulong messageId);
+        Task ChangeListAsync(IEnumerable<IUser> users, IEnumerable<string> reasons, IEnumerable<long> amounts, IEnumerable<string> from, 
+            IEnumerable<string> to, IEnumerable<ulong> guildIds, IEnumerable<ulong> channelIds, IEnumerable<ulong> messageIds);
     }
     
     public class CurrencyService : ICurrencyService
@@ -24,7 +24,7 @@ namespace Roki.Services
             _db = db;
         }
         
-        private CurrencyTransaction CreateTransaction(ulong userId, string reason, long amount, string from, ulong guildId, ulong channelId, ulong messageId) =>
+        private CurrencyTransaction CreateTransaction(ulong userId, string reason, long amount, string from, string to, ulong guildId, ulong channelId, ulong messageId) =>
             new CurrencyTransaction
             {
                 Amount = amount,
@@ -36,33 +36,35 @@ namespace Roki.Services
                 MessageId = messageId
             };
 
-        private async Task InternalChangeAsync(IUser user, string reason, long amount, string from, ulong guildId, ulong channelId, ulong messageId)
+        private async Task<bool> InternalChangeAsync(IUser user, string reason, long amount, string from, string to, ulong guildId, ulong channelId, ulong messageId)
         {
             using (var uow = _db.GetDbContext())
             {
                 var success = await uow.DUsers.UpdateCurrencyAsync(user, amount).ConfigureAwait(false);
                 if (success)
                 {
-                    var _ = CreateTransaction(user.Id, reason, amount, from, guildId, channelId, messageId);
+                    var _ = CreateTransaction(user.Id, reason, amount, from, to, guildId, channelId, messageId);
                     uow.Transaction.Add(_);
                 }
 
                 await uow.SaveChangesAsync().ConfigureAwait(false);
+                return success;
             }
         }
 
-        public async Task ChangeAsync(IUser user, string reason, long amount, string from, ulong guildId, ulong channelId, ulong messageId)
+        public async Task<bool> ChangeAsync(IUser user, string reason, long amount, string from, string to, ulong guildId, ulong channelId, ulong messageId)
         {
-            await InternalChangeAsync(user, reason, amount, from, guildId, channelId, messageId).ConfigureAwait(false);
+            return await InternalChangeAsync(user, reason, amount, from, to, guildId, channelId, messageId).ConfigureAwait(false);
         }
 
         public async Task ChangeListAsync(IEnumerable<IUser> users, IEnumerable<string> reasons, IEnumerable<long> amounts, IEnumerable<string> from,
-            IEnumerable<ulong> guildIds, IEnumerable<ulong> channelIds, IEnumerable<ulong> messageIds)
+            IEnumerable<string> to, IEnumerable<ulong> guildIds, IEnumerable<ulong> channelIds, IEnumerable<ulong> messageIds)
         {
             var usersArr = users as IUser[] ?? users.ToArray();
             var reasonsArr = reasons as string[] ?? reasons.ToArray();
             var amountsArr = amounts as long[] ?? amounts.ToArray();
             var fromArr = from as string[] ?? from.ToArray();
+            var toArr = to as string[] ?? to.ToArray();
             var guildsArr = guildIds as ulong[] ?? guildIds.ToArray();
             var chansArr = channelIds as ulong[] ?? channelIds.ToArray();
             var msgsArr = messageIds as ulong[] ?? messageIds.ToArray();
@@ -74,7 +76,7 @@ namespace Roki.Services
             {
                 for (int i = 0; i < usersArr.Length; i++)
                 {
-                    await InternalChangeAsync(usersArr[i], reasonsArr[i], amountsArr[i], fromArr[i], guildsArr[i], chansArr[i],
+                    await InternalChangeAsync(usersArr[i], reasonsArr[i], amountsArr[i], fromArr[i], toArr[i], guildsArr[i], chansArr[i],
                         msgsArr[i]).ConfigureAwait(false);
                 }
 
