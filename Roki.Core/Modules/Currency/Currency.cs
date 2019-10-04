d8c1bd1e40f1c50b04d4d7a9f08d051976d593e4
Lou <lou.zhang@mail.utoronto.ca>
2019-10-04 11:22:30 -0400

    gambling: added give command, added transferasync

--- Roki.Core/Modules/Currency/Currency.cs
+++ Roki.Core/Modules/Currency/Currency.cs
@@ -5,16 +5,19 @@ using Microsoft.EntityFrameworkCore;
 using Roki.Common.Attributes;
 using Roki.Core.Services;
 using Roki.Extensions;
+using Roki.Services;
 
 namespace Roki.Modules.Currency
 {
     public partial class Currency : RokiTopLevelModule
     {
         private readonly DbService _db;
+        private readonly CurrencyService _currency;
 
-        public Currency(DbService db)
+        public Currency(DbService db, CurrencyService currency)
         {
             _db = db;
+            _currency = currency;
         }
 
         [RokiCommand, Description, Usage, Aliases]
@@ -38,6 +41,26 @@ namespace Roki.Modules.Currency
 
                 await ctx.Channel.EmbedAsync(embed).ConfigureAwait(false);
             }
+        }
+
+        [RokiCommand, Description, Usage, Aliases]
+        [RequireContext(ContextType.Guild)]
+        public async Task Give(long amount, IGuildUser user, [Leftover] string message)
+        {
+            if (amount <= 0 || ctx.User.Id == user.Id || user.IsBot)
+                return;
+            message = $"Gift from {ctx.User.Username} - {message}";
+            var success = await _currency.TransferAsync(ctx.User, user, message, amount, ctx.Guild.Id, ctx.Channel.Id, ctx.Message.Id)
+                .ConfigureAwait(false);
+
+            if (!success)
+            {
+                await ctx.Channel.SendErrorAsync("You do not have enough stones to give.").ConfigureAwait(false);
+                return;
+            }
+
+            await ctx.Channel.EmbedAsync(new EmbedBuilder().WithOkColor()
+                .WithDescription($"{ctx.User.Username} gifted {amount} stones to {user.Username}")).ConfigureAwait(false);
         }
     }
 }
