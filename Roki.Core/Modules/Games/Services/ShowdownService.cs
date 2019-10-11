a3a31b8505251c1247fdb3c5ada54a6a9870b5a1
Lou <lou.zhang@mail.utoronto.ca>
2019-10-11 11:34:22 -0400

    showdown: finished basic showdown service

--- Roki.Core/Modules/Games/Services/ShowdownService.cs
+++ Roki.Core/Modules/Games/Services/ShowdownService.cs
@@ -9,73 +9,13 @@ namespace Roki.Modules.Games.Services
 {
     public class ShowdownService : IRService
     {
-/*        public static readonly Dictionary<string, string> actions = new Dictionary<string, string>()
-        {
-            { "debug", "ignored" },
-            { "move", "{0} used {1} on {2}." },
-            { "switch", "this is a move" },
-            { "detailschange", "this is a move" },
-            { "replace", "this is a move" },
-            { "swap", "this is a move" },
-            { "cant", "this is a move" },
-            { "faint", "this is a move" },
-            { "-fail", "this is a move" },
-            { "-notarget", "this is a move" },
-            { "-miss", "this is a move" },
-            { "-damage", "this is a move" },
-            { "-heal", "this is a move" },
-            { "-sethp", "this is a move" },
-            { "-status", "this is a move" },
-            { "-cureteam", "this is a move" },
-            { "-boost", "this is a move" },
-            { "-unboost", "this is a move" },
-            { "-setboost", "this is a move" },
-            { "-swapboost", "this is a move" },
-            { "-invertboost", "this is a move" },
-            { "-clearboost", "this is a move" },
-            { "-clearpositiveboost", "this is a move" },
-            { "-copyboost", "this is a move" },
-            { "-weather", "this is a move" },
-            { "-fieldstart", "this is a move" },
-            { "-fieldend", "this is a move" },
-            { "-sidestart", "this is a move" },
-            { "-sideend", "this is a move" },
-            { "-start", "this is a move" },
-            { "-end", "this is a move" },
-            { "-crit", "this is a move" },
-            { "-supereffective", "this is a move" },
-            { "-resisted", "this is a move" },
-            { "-immune", "this is a move" },
-            { "-item", "this is a move" },
-            { "-enditem", "this is a move" },
-            { "-ability", "this is a move" },
-            { "-endability", "this is a move" },
-            { "-transform", "this is a move" },
-            { "-mega", "this is a move" },
-            { "-primal", "this is a move" },
-            { "-burst", "this is a move" },
-            { "-zpower", "this is a move" },
-            { "-zbroken", "this is a move" },
-            { "-activate", "this is a move" },
-            { "-hint", "this is a move" },
-            { "-center", "this is a move" },
-            { "-message", "this is a move" },
-            { "-combine", "this is a move" },
-            { "-waiting", "this is a move" },
-            { "-prepare", "this is a move" },
-            { "-mustrecharge", "this is a move" },
-            { "-nothing", "this is a move" },
-            { "-hitcount", "this is a move" },
-            { "-singlemove", "this is a move" },
-            { "-singleturn", "this is a move" },
-        };*/
-
         public ShowdownService()
         {
         }
 
-        public async Task<string> StartAiGameAsync()
+        public async Task<(IEnumerable<List<string>>, IEnumerable<List<string>>)> StartAiGameAsync()
         {
+            string output;
             using (var proc = new Process())
             {
                 proc.StartInfo.FileName = "./scripts/battle.sh";
@@ -83,36 +23,39 @@ namespace Roki.Modules.Games.Services
                 proc.StartInfo.RedirectStandardOutput = true;
                 proc.Start();
                 var reader = proc.StandardOutput;
-                var output = await reader.ReadToEndAsync().ConfigureAwait(false);
+                output = await reader.ReadToEndAsync().ConfigureAwait(false);
                 proc.WaitForExit();
-
-                return output;
             }
+
+            return ParseGame(output);
         }
 
-        public async Task<string> ParseGame(string game)
+        public (IEnumerable<List<string>>, IEnumerable<List<string>>) ParseGame(string game)
         {
             var index = game.IndexOf("|\n", StringComparison.InvariantCultureIgnoreCase);
             
             var gameIntro = game.Substring(0, index);
-            var gameChunks = game.Substring(index + 1).Split("|\n");
+            var gameTurns = game.Substring(index + 1);
 
-            return "";
+            var parsedIntro = ParseIntro(gameIntro);
+            var parsedGame = ParseTurns(gameTurns);
+
+            return (parsedIntro, parsedGame);
         }
 
-        public List<List<string>> ParseIntro(string intro)
+        private static IEnumerable<List<string>> ParseIntro(string intro)
         {
             var lines = intro.Split('\n');
             var p1Poke = new List<string>();
             var p2Poke = new List<string>();
             foreach (var line in lines)
             {
-                if (line.StartsWith("|poke|p1"))
+                if (line.StartsWith("|poke|p1", StringComparison.Ordinal))
                 {
                     var poke = line.Split('|');
                     p1Poke.Add(poke[2]);
                 }
-                else if (line.StartsWith("|poke|p2"))
+                else if (line.StartsWith("|poke|p2", StringComparison.Ordinal))
                 {
                     var poke = line.Split('|');
                     p2Poke.Add(poke[2]);
@@ -173,14 +116,14 @@ namespace Roki.Modules.Games.Services
                         turnDetails.Add($"But it missed");
                         break;
                     case "-damage":
-                        turnDetails.Add(action.Length >= 4 && action[4].StartsWith("[from]")
+                        turnDetails.Add(action.Length >= 4 && action[4].StartsWith("[from]", StringComparison.Ordinal)
                             ? action[4].Contains("item:")
                                 ? $"{action[2].Substring(5)} took damage from its {action[4].Substring(12)} and is now at {action[3]} HP"
                                 : $"{action[2].Substring(5)} took damage from {action[5].Substring(5)}'s {action[4].Substring(7)} and is now at {action[3]} HP"
                             : $"{action[2].Substring(5)} took damage and is now at {action[3]} HP");
                         break;
                     case "-heal":
-                        if (action.Length >= 4 && action[4].StartsWith("[from]"))
+                        if (action.Length >= 4 && action[4].StartsWith("[from]", StringComparison.Ordinal))
                         {
                             if (action[4].Contains("item:"))
                                 turnDetails.Add($"{action[2].Substring(5)} restored health from its {action[4].Substring(13)} and is now at {action[3]} HP");
@@ -193,12 +136,12 @@ namespace Roki.Modules.Games.Services
                             turnDetails.Add($"{action[2].Substring(5)} restored health and is now at {action[3]} HP");
                         break;
                     case "-sethp":
-                        turnDetails.Add(action.Length >= 4 && action[4].StartsWith("[from]")
+                        turnDetails.Add(action.Length >= 4 && action[4].StartsWith("[from]", StringComparison.Ordinal)
                             ? $"{action[2].Substring(5)}'s HP has been set by {action[4].Substring(13)} and is now at {action[3]} HP"
                             : $"{action[2].Substring(5)}'s HP has been set to {action[3]} HP"); // should never see this
                         break;
                     case "-status":
-                        turnDetails.Add(action.Length >= 4 && action[4].StartsWith("[from]")
+                        turnDetails.Add(action.Length >= 4 && action[4].StartsWith("[from]", StringComparison.Ordinal)
                             ? action[4].Contains("move:")
                                 ? $"{action[2].Substring(5)} has been inflicted with {action[3]} from {action[4].Substring(13)}"
                                 : $"{action[2].Substring(5)} has been inflicted with {action[3]} by its {action[4].Substring(13)}"
@@ -247,7 +190,7 @@ namespace Roki.Modules.Games.Services
                     case "-weather":
                         if (action.Length == 3)
                             turnDetails.Add($"The weather is now {action[2]}");
-                        if (action[3].StartsWith("[from]"))
+                        if (action[3].StartsWith("[from]", StringComparison.Ordinal))
                         {
                             if (action[3].Contains("ability"))
                                 turnDetails.Add($"The weather is now: {action[2]} because of {action[4].Substring(10)}'s {action[3].Substring(7)}");
@@ -258,7 +201,7 @@ namespace Roki.Modules.Games.Services
                             turnDetails.Add($"The weather has cleared.");
                         break;
                     case "-fieldstart":
-                        turnDetails.Add(action[3].StartsWith("[of")
+                        turnDetails.Add(action[3].StartsWith("[of", StringComparison.Ordinal)
                             ? $"{action[3].Substring(9)}'s {action[2].Substring(6)} has effected the battlefield"
                             : $"{action[2].Substring(6)} has started from {action[4].Substring(10)}'s {action[3].Substring(16)}");
                         break;
@@ -266,19 +209,19 @@ namespace Roki.Modules.Games.Services
                         turnDetails.Add($"{action[2].Substring(6)} hsa ended");
                         break;
                     case "-sidestart":
-                        turnDetails.Add(action[3].StartsWith("move")
+                        turnDetails.Add(action[3].StartsWith("move", StringComparison.Ordinal)
                             ? $"Player {action[2][1]}'s side now has {action[3].Substring(6)}"
                             : $"Player {action[2][1]}'s side now has {action[3]}");
                         break;
                     case "-sidened":
-                        turnDetails.Add(action[3].StartsWith("move")
+                        turnDetails.Add(action[3].StartsWith("move", StringComparison.Ordinal)
                             ? $"Player {action[2][1]}'s {action[3].Substring(6)} has now ended"
                             : $"Player {action[2][1]}'s {action[3]} has now ended");
                         break;
                     case "-start":
-                        if (action[3].StartsWith("move:"))
+                        if (action[3].StartsWith("move:", StringComparison.Ordinal))
                             turnDetails.Add($"{action[2].Substring(5)} is effected by {action[3].Substring(6)}");
-                        else if (action[3].StartsWith("type"))
+                        else if (action[3].StartsWith("type", StringComparison.Ordinal))
                             turnDetails.Add($"{action[2].Substring(5)} is now a {action[4]} type, {action[5].Substring(7)}");
                         else
                             turnDetails.Add($"{action[2].Substring(5)} now has volatile status {action[3]}");
@@ -303,7 +246,7 @@ namespace Roki.Modules.Games.Services
                         break;
                     case "-enditem":
                         turnDetails.Add(action.Length >= 5
-                            ? action[5].StartsWith("[from")
+                            ? action[5].StartsWith("[from", StringComparison.Ordinal)
                                 ? $"{action[2].Substring(5)}'s {action[3]} has been destroyed by {action[4].Substring(13)}"
                                 : $"{action[2].Substring(5)}'s {action[3]} has been consumed"
                             : $"{action[2].Substring(5)}'s {action[3]} has been used");
@@ -378,7 +321,7 @@ namespace Roki.Modules.Games.Services
             return turnDetails;
         }
 
-        public List<List<string>> ParseTurns(string game)
+        private static IEnumerable<List<string>> ParseTurns(string game)
         {
             var turns = game.Split("|turn");
             return turns.Select(ParseTurn).ToList();
