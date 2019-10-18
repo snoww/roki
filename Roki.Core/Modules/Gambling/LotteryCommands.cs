0b340f8fae043bf7977ecbecb7c6e01966807be5
snow <lz@snow.xyz>
2019-10-18 14:16:02 -0400

    lottery: initial commit

---
+++ Roki.Core/Modules/Gambling/LotteryCommands.cs
@@ -0,0 +1,13 @@
+using Discord.Commands;
+
+namespace Roki.Modules.Gambling
+{
+    public partial class Gambling
+    {
+        [Group]
+        public class LotteryCommands : RokiSubmodule
+        {
+            
+        }
+    }
+}
