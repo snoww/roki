723b775c4bcfc4b59c050bf5ca16be8ca2378570
snow <lz@snow.xyz>
2019-10-18 15:17:00 -0400

    lottery: added basic joinlottery

--- Roki.Core/Modules/Gambling/LotteryCommands.cs
+++ Roki.Core/Modules/Gambling/LotteryCommands.cs
@@ -24,14 +24,30 @@ namespace Roki.Modules.Gambling
             public async Task Jackpot()
             {
                 var rokiCurr = _currency.GetCurrency(ctx.Client.CurrentUser.Id);
-                if (rokiCurr <= 0)
-                {
+                if (rokiCurr < 1000)
                     await ctx.Channel.SendErrorAsync("The lottery is currently down. Please check back another time.").ConfigureAwait(false);
-                }
 
                 await ctx.Channel.EmbedAsync(new EmbedBuilder().WithOkColor()
                         .WithTitle("Stone Lottery")
                         .WithDescription($"Current Jackpot: {Format.Bold(rokiCurr.ToString())} {Stone}"))
+                    .ConfigureAwait(false);
+            }
+
+            [RokiCommand, Description, Usage, Aliases]
+            [RequireContext(ContextType.Guild)]
+            public async Task JoinLottery()
+            {
+                var removed = await _currency.ChangeAsync(ctx.User, "Lottery Entry", -10, ctx.User.Id.ToString(), $"{ctx.Client.CurrentUser.Id}",
+                    ctx.Guild.Id, ctx.Channel.Id, ctx.Message.Id).ConfigureAwait(false);
+                if (!removed)
+                {
+                    await ctx.Channel.SendErrorAsync($"{ctx.User.Mention} you do not have enough currency to join the lottery.")
+                        .ConfigureAwait(false);
+                    return;
+                }
+
+                await ctx.Channel.EmbedAsync(new EmbedBuilder().WithOkColor()
+                        .WithDescription($"{ctx.User.Mention} you've joined the lottery."))
                     .ConfigureAwait(false);
             }
         }
