f694daf6e109c74e94c5b764cf3bb060ac277461
Lou <lou.zhang@mail.utoronto.ca>
2019-10-23 10:17:54 -0400

    store: added store command

--- Roki.Core/Services/Database/Models/Store.cs
+++ Roki.Core/Services/Database/Models/Store.cs
@@ -9,7 +9,7 @@ namespace Roki.Core.Services.Database.Models
         public string Description { get; set; }
         public string Type { get; set; }
         public long Cost { get; set; }
-        public byte Available { get; set; } = 1;
+        public int Quantity { get; set; } = 1;
         public DateTime ExpiryDate { get; set; } = DateTime.UtcNow + TimeSpan.FromDays(7);
         public DateTime ListDate { get; set; } = DateTime.UtcNow;
     }
--- Roki.Core/Services/Database/Repositories/StoreRepository.cs
+++ Roki.Core/Services/Database/Repositories/StoreRepository.cs
@@ -1,3 +1,6 @@
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
 using Roki.Core.Services.Database.Models;
 
@@ -5,13 +8,18 @@ namespace Roki.Core.Services.Database.Repositories
 {
     public interface IStoreRepository : IRepository<Store>
     {
-        
+        List<Store> GetStoreCatalog();
     }
 
     public class StoreRepository : Repository<Store>, IStoreRepository
     {
         public StoreRepository(DbContext context) : base(context)
         {
+        }
+
+        public List<Store> GetStoreCatalog()
+        {
+            return Set.OrderByDescending(c => c.Cost).AsEnumerable().ToList();
         }
     }
 }
