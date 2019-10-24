9ed6cedf3c5080a77b59dd523b0cb1ef721e0bf9
snow <lz@snow.xyz>
2019-10-24 11:20:43 -0400

    store: added listing category and type

--- Roki.Core/Services/Database/Models/Listing.cs
+++ Roki.Core/Services/Database/Models/Listing.cs
@@ -1,13 +1,17 @@
 using System;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Roki.Core.Services.Database.Models
 {
+    [Table("store")]
     public class Listing : DbEntity
     {
         public ulong SellerId { get; set; }
         public string ItemName { get; set; }
+        public string ItemDetails { get; set; }
         public string Description { get; set; } = "-";
-        public string Type { get; set; }
+        public string Category { get; set; }
+        public string Type { get; set; } = "OneTime";
         public long Cost { get; set; }
         public int Quantity { get; set; } = 1;
         public DateTime ListDate { get; set; } = DateTime.UtcNow;
