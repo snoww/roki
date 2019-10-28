0ea83902350e3cf78d92caa17dcc67f3dde39474
snow <lz@snow.xyz>
2019-10-28 12:35:46 -0400

    music: updated extensions to v5

--- Roki.Core/Modules/Music/Extensions/MusicExtensions.cs
+++ Roki.Core/Modules/Music/Extensions/MusicExtensions.cs
@@ -4,7 +4,6 @@ using System.Linq;
 using NLog;
 using Roki.Extensions;
 using Victoria;
-using Victoria.Entities;
 
 namespace Roki.Modules.Music.Extensions
 {
@@ -14,28 +13,28 @@ namespace Roki.Modules.Music.Extensions
 
         public static TimeSpan TotalPlaytime(this IEnumerable<LavaTrack> queue)
         {
-            return new TimeSpan(queue.Sum(t => t.Length.Ticks));
+            return new TimeSpan(queue.Sum(t => t.Duration.Ticks));
         }
 
         public static string PrettyTrack(this LavaTrack track)
         {
-            return $"**[{track.Title.TrimTo(65)}]({track.Uri})**";
+            return $"**[{track.Title.TrimTo(65)}]({track.Url})**";
         }
 
         public static string PrettyFullTrack(this LavaTrack track)
         {
-            return $"{track.PrettyTrack()}\n\t\t`{track.PrettyLength()} | {track.Provider.ToTitleCase()}`";
+            return $"{track.PrettyTrack()}\n\t\t`{track.PrettyLength()} | {track.Author.ToTitleCase()}`";
         }
 
         public static string PrettyFooter(this LavaTrack track, int volume)
         {
-            return $"🔉 {volume}% | {track.PrettyLength()} | {track.Provider.ToTitleCase()}";
+            return $"🔉 {volume}% | {track.PrettyLength()} | {track.Author.ToTitleCase()}";
         }
 
         public static string PrettyLength(this LavaTrack track)
         {
-            var time = track.Length.ToString(@"mm\:ss");
-            var hrs = (int) track.Length.TotalHours;
+            var time = track.Duration.ToString(@"mm\:ss");
+            var hrs = (int) track.Duration.TotalHours;
 
             if (hrs > 0)
                 return hrs + ":" + time;
