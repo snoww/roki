8b594fa999ea23f07239f988fd034f6214e8fbe6
Lou <lou.zhang@mail.utoronto.ca>
2019-10-31 10:03:00 -0400

    moderation: added block and timeout powers

--- Roki.Core/Modules/Moderation/PowerCommands.cs
+++ Roki.Core/Modules/Moderation/PowerCommands.cs
@@ -17,8 +17,7 @@ namespace Roki.Modules.Games
             [Priority(0)]
             public async Task Mute(IUser user)
             {
-                var hasPower = await _service.AvailablePower(ctx.User.Id, "Mute");
-                if (!hasPower)
+                if (!await _service.AvailablePower(ctx.User.Id, "Mute"))
                 {
                     await ctx.Channel.SendErrorAsync($"{ctx.User.Mention} do not have any mute powers available.").ConfigureAwait(false);
                     return;
@@ -26,6 +25,34 @@ namespace Roki.Modules.Games
 
                 await _service.ConsumePower(ctx.User.Id, "Mute").ConfigureAwait(false);
                 await _service.MuteUser(ctx, user as IGuildUser).ConfigureAwait(false);
+            }
+
+            [RokiCommand, Description, Usage, Aliases]
+            [RequireContext(ContextType.Guild)]
+            [Priority(0)]
+            public async Task Block(IUser user)
+            {
+                if (!await _service.AvailablePower(ctx.User.Id, "Block"))
+                {
+                    await ctx.Channel.SendErrorAsync($"{ctx.User.Mention} do not have any mute powers available.").ConfigureAwait(false);
+                    return;
+                }
+                await _service.ConsumePower(ctx.User.Id, "Block").ConfigureAwait(false);
+                await _service.BlockUser(ctx, user as IGuildUser).ConfigureAwait(false);
+            }
+            
+            [RokiCommand, Description, Usage, Aliases]
+            [RequireContext(ContextType.Guild)]
+            [Priority(0)]
+            public async Task Timeout(IUser user)
+            {
+                if (!await _service.AvailablePower(ctx.User.Id, "Timeout"))
+                {
+                    await ctx.Channel.SendErrorAsync($"{ctx.User.Mention} do not have any mute powers available.").ConfigureAwait(false);
+                    return;
+                }
+                await _service.ConsumePower(ctx.User.Id, "Timeout").ConfigureAwait(false);
+                await _service.TimeoutUser(ctx, user as IGuildUser).ConfigureAwait(false);
             }
         }
     }
