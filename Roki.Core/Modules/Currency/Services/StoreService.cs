42a5492b2b66c3c383e3617c1954b18f26b7f5f7
Lou <lou.zhang@mail.utoronto.ca>
2019-10-31 10:24:04 -0400

    gambling: fix datetime out of range on end of month days

--- Roki.Core/Modules/Currency/Services/StoreService.cs
+++ Roki.Core/Modules/Currency/Services/StoreService.cs
@@ -30,7 +30,7 @@ namespace Roki.Modules.Currency.Services
         {
             var today = DateTime.UtcNow;
             var drawToday = new DateTime(today.Year, today.Month, today.Day, 0, 0, 0);
-            var drawTmr = new DateTime(today.Year, today.Month, today.Day + 1, 0, 0, 0);
+            var drawTmr = drawToday + TimeSpan.FromDays(1);
             _timer = drawToday - today > TimeSpan.Zero 
                 ? new Timer(RemoveSubEvent, null, drawToday - today, TimeSpan.FromDays(1)) 
                 : new Timer(RemoveSubEvent, null, drawTmr - today, TimeSpan.FromDays(1));
