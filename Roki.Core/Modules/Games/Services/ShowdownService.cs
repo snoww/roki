9aa13d46b474b8ca1615e4a0c93dee89970a967b
Lou <lou.zhang@mail.utoronto.ca>
2019-11-05 18:34:22 -0500

    showdown: use contains to try fix sometimes wrong player wins

--- Roki.Core/Modules/Games/Services/ShowdownService.cs
+++ Roki.Core/Modules/Games/Services/ShowdownService.cs
@@ -67,8 +67,8 @@ namespace Roki.Modules.Games.Services
         {
             var gameId = await GetBetPokemonGame(uid).ConfigureAwait(false);
             var p1 = await File.ReadAllLinesAsync($@"./logs/1-{gameId}.log").ConfigureAwait(false);
-            var winner = p1.Any(l => l.StartsWith("|win|rokibot1", StringComparison.OrdinalIgnoreCase)) ? 2 : 1;
-            File.Delete($@"./logs/1-{gameId}.log");
+            var winner = p1.Any(l => l.Contains("|win|rokibot1", StringComparison.OrdinalIgnoreCase)) ? 2 : 1;
+//            File.Delete($@"./logs/1-{gameId}.log");
             File.Delete($@"./logs/rokibot-{gameId}.log");
             return winner;
         }
