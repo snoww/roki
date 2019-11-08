cf14bfbb5cc8430c6a5a9ae563ddb5a8a0c3fba2
snow <lz@snow.xyz>
2019-11-08 15:15:33 -0500

    stocks: added service methods for trading

--- Roki.Core/Services/Database/Models/Trades.cs
+++ Roki.Core/Services/Database/Models/Trades.cs
@@ -7,6 +7,7 @@ namespace Roki.Core.Services.Database.Models
         public ulong UserId { get; set; }
         public string Symbol { get; set; }
         public string Position { get; set; }
+        public string Action { get; set; }
         public long Shares { get; set; }
         public decimal PurchasePrice { get; set; }
         public DateTime TransactionDate { get; set; }
