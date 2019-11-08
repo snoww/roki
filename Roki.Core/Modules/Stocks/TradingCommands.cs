5dd1f6ee52f4d8ae5b8dff9d7e9671e0b2b6f6d7
snow <lz@snow.xyz>
2019-11-08 15:50:06 -0500

    stocks: added stocksell command

--- Roki.Core/Modules/Stocks/TradingCommands.cs
+++ Roki.Core/Modules/Stocks/TradingCommands.cs
@@ -37,7 +37,7 @@ namespace Roki.Modules.Stocks
                 }
 
                 var cost = (long) Math.Ceiling(amount * price.Value);
-                var removed = await _service.UpdateInvAccountAsync(ctx.User.Id, cost).ConfigureAwait(false);
+                var removed = await _service.UpdateInvAccountAsync(ctx.User.Id, -cost).ConfigureAwait(false);
                 if (!removed)
                 {
                     await ctx.Channel.SendErrorAsync("You do not have enough in your Investing Account to invest").ConfigureAwait(false);
@@ -52,6 +52,41 @@ namespace Roki.Modules.Stocks
                 else
                     embed.WithDescription($"{ctx.User.Mention}\n You've successfully purchased {amount} shares of {symbol.ToUpper()} at {price.Value}\n" +
                                           $"Total Cost: {cost} {_roki.Properties.CurrencyIcon}");
+
+                await ctx.Channel.EmbedAsync(embed).ConfigureAwait(false);
+            }
+            
+            [RokiCommand, Usage, Description, Aliases]
+            public async Task StockSell(string symbol, long amount)
+            {
+                if (amount <= 0)
+                {
+                    await ctx.Channel.SendErrorAsync("Need to sell at least 1 share").ConfigureAwait(false);
+                    return;
+                }
+
+                var price = await _service.GetLatestPriceAsync(symbol).ConfigureAwait(false);
+                if (price == null)
+                {
+                    await ctx.Channel.SendErrorAsync("Unknown Symbol").ConfigureAwait(false);
+                    return;
+                }
+
+                var cost = (long) Math.Ceiling(amount * price.Value);
+                await _service.UpdateInvAccountAsync(ctx.User.Id, cost).ConfigureAwait(false);
+                var success = await _service.UpdateUserPortfolioAsync(ctx.User.Id, symbol, Position.Long, "sell", price.Value, amount).ConfigureAwait(false);
+                if (!success)
+                {
+                    await ctx.Channel.SendErrorAsync($"You do not have that many shares to sell.").ConfigureAwait(false);
+                    return;
+                }
+                var embed = new EmbedBuilder().WithOkColor();
+                if (amount == 1)
+                    embed.WithDescription($"{ctx.User.Mention}\n You've successfully sold 1 share of {symbol.ToUpper()} at {price.Value}\n" +
+                                          $"Total sold for: {cost} {_roki.Properties.CurrencyIcon}");
+                else
+                    embed.WithDescription($"{ctx.User.Mention}\n You've successfully sold {amount} shares of {symbol.ToUpper()} at {price.Value}\n" +
+                                          $"Total sold for: {cost} {_roki.Properties.CurrencyIcon}");
 
                 await ctx.Channel.EmbedAsync(embed).ConfigureAwait(false);
             }
