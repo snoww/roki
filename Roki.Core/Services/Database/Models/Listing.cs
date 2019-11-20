15095e275f8e4fd398512dd79d14bdbecf7ebfbd
snow <lz@snow.xyz>
2019-11-20 15:47:15 -0500

    db: added all column attributes in lowercase

--- Roki.Core/Services/Database/Models/Listing.cs
+++ Roki.Core/Services/Database/Models/Listing.cs
@@ -12,12 +12,17 @@ namespace Roki.Core.Services.Database.Models
         public string ItemName { get; set; }
         [Column("details")]
         public string ItemDetails { get; set; }
+        [Column("description")]
         public string Description { get; set; } = "-";
+        [Column("category")]
         public string Category { get; set; }
+        [Column("type")]
         public string Type { get; set; } = "OneTime";
         [Column("subscription_days")]
         public int? SubscriptionDays { get; set; }
+        [Column("cost")]
         public long Cost { get; set; }
+        [Column("quantity")]
         public int Quantity { get; set; } = 1;
         [Column("list_date")]
         public DateTimeOffset ListDate { get; set; } = DateTimeOffset.UtcNow;
