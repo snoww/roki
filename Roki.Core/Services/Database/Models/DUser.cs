525196c4e86a5fa2396ef27960bfeb84f525e41d
snow <lz@snow.xyz>
2019-11-20 11:09:52 -0500

    db: added foreign keys

--- Roki.Core/Services/Database/Models/DUser.cs
+++ Roki.Core/Services/Database/Models/DUser.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Roki.Core.Services.Database.Models
@@ -6,25 +7,35 @@ namespace Roki.Core.Services.Database.Models
     [Table("users")]
     public class DUser : DbEntity
     {
-        [Column("user_id")]
+        public DUser()
+        {
+            Lottery = new HashSet<Lottery>();
+            Quotes = new HashSet<Quote>();
+            Store = new HashSet<Listing>();
+            Subscriptions = new HashSet<Subscriptions>();
+            Trades = new HashSet<Trades>();
+        }
+        
         public ulong UserId { get; set; }
         public string Username { get; set; }
         public string Discriminator { get; set; }
-        [Column("avatar_id")]
         public string AvatarId { get; set; }
-        [Column("total_xp")]
         public int TotalXp { get; set; }
-        [Column("last_level_up")]
         public DateTimeOffset LastLevelUp { get; set; } = DateTimeOffset.UtcNow;
-        [Column("last_xp_gain")]
         public DateTimeOffset LastXpGain { get; set; } = DateTimeOffset.UtcNow;
-        [Column("notification_location")]
         public string NotificationLocation { get; set; } = "dm";
         public long Currency { get; set; } = 0;
         public string Inventory { get; set; } = null;
         [Column("investing")]
         public decimal InvestingAccount { get; set; } = 50000;
         public string Portfolio { get; set; } = null;
+        
+        public ICollection<Lottery> Lottery { get; set; }
+        public ICollection<Quote> Quotes { get; set; }
+        public ICollection<Listing> Store { get; set; }
+        public ICollection<Subscriptions> Subscriptions { get; set; }
+        public ICollection<Trades> Trades { get; set; }
+
         
         public override bool Equals(object obj) => 
             obj is DUser dUser && dUser.UserId == UserId;
--- Roki.Core/Services/Database/Models/Listing.cs
+++ Roki.Core/Services/Database/Models/Listing.cs
@@ -6,7 +6,6 @@ namespace Roki.Core.Services.Database.Models
     [Table("store")]
     public class Listing : DbEntity
     {
-        [Column("seller_id")]
         public ulong SellerId { get; set; }
         [Column("name")]
         public string ItemName { get; set; }
@@ -15,11 +14,11 @@ namespace Roki.Core.Services.Database.Models
         public string Description { get; set; } = "-";
         public string Category { get; set; }
         public string Type { get; set; } = "OneTime";
-        [Column("subscription_days")]
         public int? SubscriptionDays { get; set; }
         public long Cost { get; set; }
         public int Quantity { get; set; } = 1;
-        [Column("list_date")]
         public DateTimeOffset ListDate { get; set; } = DateTimeOffset.UtcNow;
+        
+        public virtual DUser Seller { get; set; }
     }
 }
