23cd96d983906ec7f256a55af5b1429f5ea540c3
Lou <lou.zhang@mail.utoronto.ca>
2019-11-21 09:13:27 -0500

    store: update rr every 30 minutes

--- Roki.Core/Services/Database/RokiContext.cs
+++ Roki.Core/Services/Database/RokiContext.cs
@@ -4,21 +4,6 @@ using Roki.Core.Services.Database.Models;
 
 namespace Roki.Core.Services.Database
 {
-//    public class RokiContextFactory : IDesignTimeDbContextFactory<RokiContext>
-//    {
-//        public RokiContext CreateDbContext(string[] args)
-//        {
-//            var optionsBuilder = new DbContextOptionsBuilder<RokiContext>();
-//            IRokiConfig config = new RokiConfig();
-//            var builder = new SqliteConnectionStringBuilder(config.Db.ConnectionString);
-//            builder.DataSource = Path.Combine(Directory.GetCurrentDirectory(), builder.DataSource);
-//            optionsBuilder.UseSqlite(builder.ToString());
-//            var ctx = new RokiContext(optionsBuilder.Options);
-//            ctx.Database.SetCommandTimeout(60);
-//            return ctx;
-//        }
-//    }
-
     public class RokiContext : DbContext
     {
         public RokiContext(DbContextOptions options) : base(options)
@@ -28,11 +13,6 @@ namespace Roki.Core.Services.Database
         public DbSet<Quote> Quotes { get; set; }
         public DbSet<DUser> DUsers { get; set; }
         public DbSet<DMessage> DMessages { get; set; }
-
-//        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-//        {
-//            optionsBuilder.UseMySQL("server=localhost;database=roki;user=roki;password=roki");
-//        }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
