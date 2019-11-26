e4ecf337ca4141a10a2109eba79393433add99d4
snow <lz@snow.xyz>
2019-11-26 11:50:08 -0500

    events: added guild and channel models for db

---
+++ Roki.Core/Services/Database/Models/Channel.cs
@@ -0,0 +1,25 @@
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Roki.Core.Services.Database.Models
+{
+    [Table("channels")]
+    public class Channel : DbEntity
+    {
+        [Column("channel_id")]
+        public ulong ChannelId { get; set; }
+        [Column("name")]
+        public string Name { get; set; }
+        [Column("guild_id")]
+        public ulong GuildId { get; set; }
+        [Column("guild_name")]
+        public string GuildName { get; set; }
+        [Column("user_count")]
+        public int UserCount { get; set; }
+        [Column("is_nsfw")]
+        public bool IsNsfw { get; set; }
+        [Column("currency_generation")]
+        public bool CurrencyGeneration { get; set; }
+        [Column("xp_gain")]
+        public bool XpGain { get; set; }
+    }
+}
