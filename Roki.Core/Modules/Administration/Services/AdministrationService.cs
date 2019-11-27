2177c0537f6609ccfd7b0be28806f52e5d93e2f1
Lou <lou.zhang@mail.utoronto.ca>
2019-11-26 20:46:10 -0500

    roki: improved fill command by not caring about id

--- Roki.Core/Modules/Administration/Services/AdministrationService.cs
+++ Roki.Core/Modules/Administration/Services/AdministrationService.cs
@@ -4,6 +4,7 @@ using Discord;
 using Discord.Commands;
 using Discord.WebSocket;
 using Roki.Core.Services;
+using Roki.Extensions;
 
 namespace Roki.Modules.Administration.Services 
 {
@@ -21,22 +22,19 @@ namespace Roki.Modules.Administration.Services
             if (!(ctx.Channel is SocketTextChannel channel)) return;
             var messages = await channel.GetMessagesAsync(messageId, Direction.After, 10000).FlattenAsync().ConfigureAwait(false);
             using var uow = _db.GetDbContext();
-            int i = 1;
+            var count = 1;
             foreach (var message in messages)
             {
                 if (message.Author.IsBot) continue;
-                i++;
-                Console.Write(i + ": ");
                 if (await uow.Messages.MessageExists(message.Id).ConfigureAwait(false))
                 {
-                    Console.WriteLine($"[{message.Id}] exists");
                     continue;
                 }
-                await uow.Messages.AddToTempTableAsync(message).ConfigureAwait(false);
-                Console.WriteLine($"[{message.Id}] added");
+                await uow.Messages.AddMissingMessageAsync(message).ConfigureAwait(false);
+                count++;
             }
-            await uow.Messages.MoveToTempTableAsync(messageId).ConfigureAwait(false);
             await uow.SaveChangesAsync().ConfigureAwait(false);
+            await ctx.Channel.SendMessageAsync($"{count} messages have been added");
         }
     }
 }
