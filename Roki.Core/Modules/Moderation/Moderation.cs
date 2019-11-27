5a4bb58ea36d05a1de77a638bb2c58fd371d983c
snow <lz@snow.xyz>
2019-11-27 09:54:10 -0500

    mod: added command to enable/disable logging for a channel

--- Roki.Core/Modules/Moderation/Moderation.cs
+++ Roki.Core/Modules/Moderation/Moderation.cs
@@ -2,10 +2,12 @@ using System.Threading.Tasks;
 using Discord;
 using Discord.Commands;
 using Roki.Common.Attributes;
+using Roki.Extensions;
+using Roki.Modules.Moderation.Services;
 
 namespace Roki.Modules.Moderation
 {
-    public partial class Moderation : RokiTopLevelModule
+    public partial class Moderation : RokiTopLevelModule<ModerationService>
     {
         [RokiCommand, Description, Usage, Aliases]
         [RequireContext(ContextType.Guild)]
@@ -14,6 +16,17 @@ namespace Roki.Modules.Moderation
         {
             await ctx.Channel.SendMessageAsync(
                 $"UserId: `{user.Id}`\nUsername: {user.Username}\nDiscriminator: `{user.Discriminator}`\nAvatarId: `{user.AvatarId}`\n").ConfigureAwait(false);
+        }
+
+        [RokiCommand, Description, Usage, Aliases]
+        [RequireContext(ContextType.Guild)]
+        [RequireUserPermission(GuildPermission.ManageMessages)]
+        public async Task Logging(bool enable = true)
+        {
+            await _service.LoggingChannel(ctx.Channel.Id, enable);
+            await ctx.Channel.EmbedAsync(new EmbedBuilder().WithOkColor()
+                    .WithDescription("This channel's messages will now be logged.\nUse `.logging false` to disable logging"))
+                .ConfigureAwait(false);
         }
     }
 }
---
+++ Roki.Core/Modules/Moderation/Services/ModerationService.cs
@@ -0,0 +1,24 @@
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Roki.Core.Services;
+
+namespace Roki.Modules.Moderation.Services
+{
+    public class ModerationService : IRService
+    {
+        private readonly DbService _db;
+
+        public ModerationService(DbService db)
+        {
+            _db = db;
+        }
+
+        public async Task LoggingChannel(ulong channelId, bool enable)
+        {
+            using var uow = _db.GetDbContext();
+            var channel = await uow.Context.Channels.FirstAsync(c => c.ChannelId == channelId);
+            channel.Logging = enable;
+            await uow.SaveChangesAsync().ConfigureAwait(false);
+        }
+    }
+}
