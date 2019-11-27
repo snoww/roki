ac1c641771350b3715da22d1d30439c47cf27b43
snow <lz@snow.xyz>
2019-11-27 12:43:40 -0500

    gambling: updated coin commands to show balance after game

---
+++ Roki.Core/Modules/Gambling/Services/GamblingService.cs
@@ -0,0 +1,20 @@
+using Roki.Core.Services;
+
+namespace Roki.Modules.Gambling.Services
+{
+    public class GamblingService : IRService
+    {
+        private readonly DbService _db;
+
+        public GamblingService(DbService db)
+        {
+            _db = db;
+        }
+
+        public long GetCurrencyAsync(ulong userId)
+        {
+            using var uow = _db.GetDbContext();
+            return uow.Users.GetUserCurrency(userId);
+        }
+    }
+}
