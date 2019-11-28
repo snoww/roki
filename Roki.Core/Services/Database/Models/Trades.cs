75e28c9103d6777b24e2793d9a67c6c4f3c99779
Lou <lou.zhang@mail.utoronto.ca>
2019-11-28 09:52:06 -0500

    db: add pk back for ef

--- Roki.Core/Services/Database/Models/Trades.cs
+++ Roki.Core/Services/Database/Models/Trades.cs
@@ -1,4 +1,5 @@
 using System;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Roki.Core.Services.Database.Models
@@ -6,6 +7,9 @@ namespace Roki.Core.Services.Database.Models
     [Table("trades")]
     public class Trades : DbEntity
     {
+        [Key]
+        [Column("id")]
+        public int Id { get; set; }
         [Column("user_id")]
         public ulong UserId { get; set; }
         [Column("symbol")]
