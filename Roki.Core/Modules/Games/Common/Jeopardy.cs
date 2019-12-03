3faf2f0a8cc16ffe4960b8f90ca13cc6a621dde2
Lou <lou.zhang@mail.utoronto.ca>
2019-12-03 13:35:31 -0500

    jpdy: added queries for jeopardy questions

--- Roki.Core/Modules/Games/Common/Jeopardy.cs
+++ Roki.Core/Modules/Games/Common/Jeopardy.cs
@@ -1,7 +1,13 @@
+using Roki.Core.Services;
+
 namespace Roki.Modules.Games.Common
 {
     public class Jeopardy
     {
-        
+        private readonly DbService _db;
+        public Jeopardy(DbService db)
+        {
+            _db = db;
+        }
     }
 }
---
+++ Roki.Core/Modules/Games/Common/JeopardyModel.cs
@@ -0,0 +1,10 @@
+namespace Roki.Modules.Games.Common
+{
+    public class JeopardyModel
+    {
+        public string Category { get; set; }
+        public string Clue { get; set; }
+        public string Answer { get; set; }
+        public int Value { get; set; }
+    }
+}
