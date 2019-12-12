3817c4d77b6dd5710002c0dc13e68adfd1605b1e
snow <lz@snow.xyz>
2019-12-12 16:10:26 -0500

    jpdy: added guess handler, stop check

--- Roki.Core/Modules/Games/Common/Jeopardy.cs
+++ Roki.Core/Modules/Games/Common/Jeopardy.cs
@@ -9,6 +9,7 @@ using Discord;
 using Discord.Commands;
 using Discord.WebSocket;
 using Microsoft.EntityFrameworkCore.Internal;
+using NLog;
 using Roki.Common;
 using Roki.Core.Services;
 using Roki.Extensions;
@@ -19,8 +20,8 @@ namespace Roki.Modules.Games.Common
     {
         private SemaphoreSlim _guess = new SemaphoreSlim(1, 1);
         private readonly DbService _db;
+        private readonly Logger _log;
         private readonly DiscordSocketClient _client;
-        private readonly ICommandContext _ctx;
         private readonly Dictionary<string, List<JClue>> _clues;
 
         public IGuild Guild { get; }
@@ -38,20 +39,20 @@ namespace Roki.Modules.Games.Common
 //        private Dictionary<int, bool> _choices1 = new Dictionary<int, bool> {{200, false},{400, false},{600, false},{800, false},{1000, false}};
 //        private Dictionary<int, bool> _choices2 = new Dictionary<int, bool> {{200, false},{400, false},{600, false},{800, false},{1000, false}};
         
-        public Jeopardy(DbService db, DiscordSocketClient client, Dictionary<string, List<JClue>> clues, IGuild guild, ITextChannel channel, ICommandContext ctx)
+        public Jeopardy(DbService db, DiscordSocketClient client, Dictionary<string, List<JClue>> clues, IGuild guild, ITextChannel channel)
         {
+            _log = LogManager.GetCurrentClassLogger();
             _db = db;
             _client = client;
             _clues = clues;
             
             Guild = guild;
             Channel = channel;
-            _ctx = ctx;
         }
 
         public async Task StartGame()
         {
-            await _ctx.Channel.EmbedAsync(new EmbedBuilder().WithColor(Color.Blue)
+            await Channel.EmbedAsync(new EmbedBuilder().WithColor(Color.Blue)
                     .WithTitle("Jeopardy!")
                     .WithDescription(
                         "Welcome to Jeopardy!\nTo choose a category, please use the format `category for xxx`, you must specify full category name.\nResponses must be in question form"))
@@ -66,15 +67,15 @@ namespace Roki.Modules.Games.Common
                 while (catStatus != CategoryStatus.Success)
                 {
                     if (catStatus == CategoryStatus.UnavailableClue)
-                        await _ctx.Channel.SendErrorAsync("That clue is not available, please try again").ConfigureAwait(false);
+                        await Channel.SendErrorAsync("That clue is not available, please try again").ConfigureAwait(false);
                     else if (catStatus == CategoryStatus.WrongAmount)
-                        await _ctx.Channel.SendErrorAsync("There are no clues available for that amount, please try again")
+                        await Channel.SendErrorAsync("There are no clues available for that amount, please try again")
                             .ConfigureAwait(false);
                     else if (catStatus == CategoryStatus.WrongCategory)
-                        await _ctx.Channel.SendErrorAsync("No such category found, please try again").ConfigureAwait(false);
+                        await Channel.SendErrorAsync("No such category found, please try again").ConfigureAwait(false);
                     else
                     {
-                        await _ctx.Channel.SendErrorAsync("No response received, stopped Jeopardy! game.").ConfigureAwait(false);
+                        await Channel.SendErrorAsync("No response received, stopped Jeopardy! game.").ConfigureAwait(false);
                         return;
                     }
                     
@@ -83,25 +84,56 @@ namespace Roki.Modules.Games.Common
                 }
                 
                 // CurrentClue is now the chosen clue
-                await _ctx.Channel.EmbedAsync(new EmbedBuilder().WithColor(Color.Blue)
+                await Channel.EmbedAsync(new EmbedBuilder().WithColor(Color.Blue)
+                        .WithAuthor("Jeopardy!")
                         .WithTitle($"{CurrentClue.Category} - {CurrentClue.Value}")
                         .WithDescription(CurrentClue.Clue))
                     .ConfigureAwait(false);
 
                 try
                 {
-                    _client.MessageReceived += Guesses;
+                    _client.MessageReceived += GuessHandler;
                     CanGuess = true;
+                    await Task.Delay(TimeSpan.FromSeconds(30)).ConfigureAwait(false);
                 }
                 finally
                 {
                     CanGuess = false;
-                    _client.MessageReceived -= Guesses;
+                    _client.MessageReceived -= GuessHandler;
+                }
+
+                if (!_cancel.IsCancellationRequested)
+                {
+                    await Channel.EmbedAsync(new EmbedBuilder().WithColor(Color.Red)
+                            .WithAuthor("Jeopardy!")
+                            .WithTitle("Times Up!")
+                            .WithDescription($"The correct answer was: `{CurrentClue.Answer}`"))
+                        .ConfigureAwait(false);
+                    if (++_timeout >= 2)
+                        await StopJeopardyGame().ConfigureAwait(false);
                 }
             }
 
         }
 
+        private async Task EnsureStopped()
+        {
+            StopGame = true;
+            await Channel.EmbedAsync(new EmbedBuilder().WithColor(Color.Blue)
+                    .WithAuthor("Jeopardy!")
+                    .WithTitle("Final Results")
+                    .WithDescription("Leaderboard"))
+                .ConfigureAwait(false);
+        }
+
+        private async Task StopJeopardyGame()
+        {
+            var old = StopGame;
+            StopGame = true;
+            if (!old)
+                await Channel.SendErrorAsync("Jeopardy! game stopped. No answers received.").ConfigureAwait(false);
+        }
+        
         private async Task ShowCategories()
         {
             var embed = new EmbedBuilder().WithColor(Color.Blue)
@@ -136,27 +168,41 @@ namespace Roki.Modules.Games.Common
             return CategoryStatus.Success;
         }
 
-        private Task Guesses(SocketMessage msg)
+        private Task GuessHandler(SocketMessage msg)
         {
             var _ = Task.Run(async () =>
             {
-                if (msg.Author.IsBot || msg.Channel != Channel || !Regex.IsMatch(msg.Content, "^what|where|who")) return;
-                var guess = false;
-                await _guess.WaitAsync().ConfigureAwait(false);
                 try
                 {
-                    if (CanGuess && CurrentClue.CheckAnswer(msg.Content) && !_cancel.IsCancellationRequested)
+                    if (msg.Author.IsBot || msg.Channel != Channel || !Regex.IsMatch(msg.Content, "^what|where|who")) return;
+                    var guess = false;
+                    await _guess.WaitAsync().ConfigureAwait(false);
+                    try
                     {
-                        Users.AddOrUpdate(msg.Author, CurrentClue.Value, (u, old) => old + CurrentClue.Value);
-                        guess = true;
+                        if (CanGuess && CurrentClue.CheckAnswer(msg.Content) && !_cancel.IsCancellationRequested)
+                        {
+                            Users.AddOrUpdate(msg.Author, CurrentClue.Value, (u, old) => old + CurrentClue.Value);
+                            guess = true;
+                        }
                     }
+                    finally
+                    {
+                        _guess.Release();
+                    }
+                    if (!guess) return;
+                    _cancel.Cancel();
+
+                    await Channel.EmbedAsync(new EmbedBuilder().WithColor(Color.Blue)
+                            .WithAuthor("Jeopardy!")
+                            .WithTitle($"{CurrentClue.Category} - {CurrentClue.Value}")
+                            .WithDescription($"{msg.Author.Mention} Correct.\nThe correct answer was: `{CurrentClue.Answer}`\n" +
+                                             $"Your total score is `{Users[msg.Author]:N0}`"))
+                        .ConfigureAwait(false);
                 }
-                finally
+                catch (Exception e)
                 {
-                    _guess.Release();
+                    _log.Warn(e);
                 }
-                if (!guess) return;
-                _cancel.Cancel();
             });
             return Task.CompletedTask;
         }
