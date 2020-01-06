634ace17f18bdd47bac589f6663be51f1a443229
Lou <lou.zhang@mail.utoronto.ca>
2020-01-06 10:52:39 -0500

    tts: touch up tts module

--- Roki.Core/Modules/Music/TextToSpeechCommands.cs
+++ Roki.Core/Modules/Music/TextToSpeechCommands.cs
@@ -18,8 +18,7 @@ namespace Roki.Modules.Music
         [Group]
         public class TextToSpeechCommands : RokiSubmodule
         {
-            private readonly string _api = "https://streamlabs.com/polly/speak";
-            private string _queryJson = @"{""voice"": ""Brian"", ""text"": ""textString""}";
+            private const string StreamlabsApi = "https://streamlabs.com/polly/speak";
             private static readonly JsonSerializerOptions Options = new JsonSerializerOptions{PropertyNameCaseInsensitive = true};
             
             [RokiCommand, Description, Usage, Aliases]
@@ -40,52 +39,66 @@ namespace Roki.Modules.Music
                     return;
                 }
 
-                using (var http = new HttpClient())
+                using var http = new HttpClient();
+                var queryRequest = JsonSerializer.Serialize(new TtsRequest {Text = query});
+                var content = new StringContent(queryRequest, Encoding.UTF8, "application/json");
+                var result = await http.PostAsync(StreamlabsApi, content).ConfigureAwait(false);
+
+                if (!result.IsSuccessStatusCode)
                 {
-                    var queryString = _queryJson.Replace("textString", query);
-                    var content = new StringContent(queryString, Encoding.UTF8, "application/json");
-                    var result = await http.PostAsync(_api, content).ConfigureAwait(false);
-
-                    var tts = JsonSerializer.Deserialize<TtsModel>(await result.Content.ReadAsStringAsync().ConfigureAwait(false), Options);
-                    if (tts.Success)
-                    {
-                        var guid = Guid.NewGuid().ToString().Substring(0, 7);
-                        using (var client = new WebClient())
-                        {
-                            await client.DownloadFileTaskAsync(tts.SpeakUrl, $"./temp/tts-{guid}.ogg").ConfigureAwait(false);
-                        }
-
-                        using (var proc = new Process())
-                        {
-                            proc.StartInfo = new ProcessStartInfo
-                            {
-                                FileName = "ffmpeg",
-                                Arguments = $"-i ./temp/tts-{guid}.ogg ./temp/tts-{guid}.mp3",
-                                RedirectStandardOutput = false,
-                                UseShellExecute = false,
-                                CreateNoWindow = true,
-                            };
-                            proc.Start();
-                            proc.WaitForExit();
-                        }
-
-                        await ctx.Channel.SendFileAsync($"./temp/tts-{guid}.mp3").ConfigureAwait(false);
-                        File.Delete($"./temp/tts-{guid}.ogg");
-                        File.Delete($"./temp/tts-{guid}.mp3");
-                    }
-                    else
-                    {
-                        await ctx.Channel.SendErrorAsync("Something went wrong with the TTS.").ConfigureAwait(false);
-                    }
+                    await ctx.Channel.SendErrorAsync("Streamlabs TTS error. Please try again later.").ConfigureAwait(false);
+                    return;
                 }
-                
+
+                var tts = JsonSerializer.Deserialize<TtsModel>(await result.Content.ReadAsStringAsync().ConfigureAwait(false), Options);
+                if (tts.Success)
+                {
+                    var guid = Guid.NewGuid().ToString().Substring(0, 7);
+                    var fileName = $"{ctx.User.Username}-{guid}";
+                    using (var client = new WebClient())
+                    {
+                        await client.DownloadFileTaskAsync(tts.SpeakUrl, $"./temp/{fileName}.ogg").ConfigureAwait(false);
+                    }
+
+                    using (var proc = new Process())
+                    {
+                        proc.StartInfo = new ProcessStartInfo
+                        {
+                            FileName = "ffmpeg",
+                            Arguments = $"-i ./temp/tts-{guid}.ogg ./temp/tts-{guid}.mp3",
+                            RedirectStandardOutput = false,
+                            UseShellExecute = false,
+                            CreateNoWindow = true,
+                        };
+                        proc.Start();
+                        proc.WaitForExit();
+                    }
+
+                    await ctx.Channel.SendFileAsync($"./temp/{fileName}.mp3").ConfigureAwait(false);
+                    File.Delete($"./temp/{fileName}.ogg");
+                    File.Delete($"./temp/{fileName}.mp3");
+                }
+                else
+                {
+                    await ctx.Channel.SendErrorAsync("Streamlabs TTS error. Please try again later.").ConfigureAwait(false);
+                }
             }
         }
     }
 
+    public class TtsRequest
+    {
+        [JsonPropertyName("voice")]
+        public string Voice { get; set; } = "Brian";
+        
+        [JsonPropertyName("text")]
+        public string Text { get; set; }
+    }
+    
     public class TtsModel
     {
         public bool Success { get; set; }
+        
         [JsonPropertyName("speak_url")]
         public string SpeakUrl { get; set; }
     }
