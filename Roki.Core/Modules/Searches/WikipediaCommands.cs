925a924315ad3a01ef656f08166619566a4fd889
Lou <lou.zhang@mail.utoronto.ca>
2020-01-07 16:00:02 -0500

    searches: only encode title

--- Roki.Core/Modules/Searches/WikipediaCommands.cs
+++ Roki.Core/Modules/Searches/WikipediaCommands.cs
@@ -63,7 +63,7 @@ namespace Roki.Modules.Searches
                     .WithAuthor("Wikipedia", WikipediaIconUrl)
                     .WithTitle($"Search results for: `{query}`")
                     .WithDescription(string.Join("\n", results
-                        .Select(a => $"{counter++}. [{a.Title}]({HttpUtility.UrlEncode($"{WikipediaUrl}/{a.Title}")})\n\t{a.Snippet}")));
+                        .Select(a => $"{counter++}. [{a.Title}]({WikipediaUrl}/{HttpUtility.UrlEncode(a.Title)})\n\t{a.Snippet}")));
 
                 await ctx.Channel.EmbedAsync(embed).ConfigureAwait(false);
                 // for future allow selecting article and showing it
@@ -72,7 +72,7 @@ namespace Roki.Modules.Searches
             private async Task SendArticleAsync(WikiSummary article)
             {
                 var embed = new EmbedBuilder().WithOkColor()
-                    .WithAuthor("Wikipedia", WikipediaIconUrl, HttpUtility.UrlEncode($"{WikipediaUrl}/{article.Title}"))
+                    .WithAuthor("Wikipedia", WikipediaIconUrl, $"{WikipediaUrl}/{HttpUtility.UrlEncode(article.Title)}")
                     .WithTitle(article.Title)
                     .WithDescription(article.Extract);
                 
