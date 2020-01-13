1425f5fc7c708c2a560eadeeeb1f08cf62c6a9e0
snow <lz@snow.xyz>
2020-01-13 09:12:58 -0500

    xp: misc changes in xp

--- Roki.Core/Modules/Xp/Xp.cs
+++ Roki.Core/Modules/Xp/Xp.cs
@@ -21,7 +21,7 @@ namespace Roki.Modules.Xp
         }
         
         [RokiCommand, Description, Usage, Aliases]
-        public async Task Experience([Leftover] IUser user = null)
+        public async Task Experience(IUser user = null)
         {
             user ??= ctx.User;
             using var uow = _db.GetDbContext();
@@ -31,42 +31,42 @@ namespace Roki.Modules.Xp
         }
 
         [RokiCommand, Description, Usage, Aliases]
-        public async Task XpLeaderboard([Leftover] int page = 0)
+        public async Task XpLeaderboard(int page = 0)
         {
             if (page < 0)
                 return;
             if (page > 0)
                 page -= 1;
-            using (var uow = _db.GetDbContext())
+            using var uow = _db.GetDbContext();
+            var list = uow.Users.GetUsersXpLeaderboard(page);
+            var embed = new EmbedBuilder().WithOkColor()
+                .WithTitle("XP Leaderboard");
+            var i = 9 * page + 1;
+            foreach (var user in list)
             {
-                var list = uow.Users.GetUsersXpLeaderboard(page);
-                var embed = new EmbedBuilder().WithOkColor()
-                    .WithTitle("XP Leaderboard");
-                var i = 9 * page + 1;
-                foreach (var user in list)
-                {
-                    embed.AddField($"#{i++} {user.Username}#{user.Discriminator}", $"Level `{new XpLevel(user.TotalXp).Level:N0}` - `{user.TotalXp:N0}` xp");
-                }
-
-                await ctx.Channel.EmbedAsync(embed).ConfigureAwait(false);
+                embed.AddField($"#{i++} {user.Username}#{user.Discriminator}", $"Level `{new XpLevel(user.TotalXp).Level:N0}` - `{user.TotalXp:N0}` xp");
             }
+
+            await ctx.Channel.EmbedAsync(embed).ConfigureAwait(false);
         }
 
         [RokiCommand, Description, Usage, Aliases]
         [RequireContext(ContextType.Guild)]
         public async Task XpNotification(string notify = null, IUser user = null)
         {
-            var caller = ctx.User as SocketGuildUser;
+            var caller = (IGuildUser) ctx.User;
             var isAdmin = false;
-            if (user != null && !caller.GuildPermissions.Administrator)
-                return;
-            if (user != null && caller.GuildPermissions.Administrator)
+            if (user != null)
+            {
+                if (!caller.GuildPermissions.Administrator)
+                    return;
+                
                 isAdmin = true;
-            
+            }
+
             if (string.IsNullOrWhiteSpace(notify))
             {
-                await ctx.Channel
-                    .EmbedAsync(new EmbedBuilder().WithErrorColor().WithDescription("Please provide notification location for xp: none, dm, server."))
+                await ctx.Channel.SendErrorAsync("Please provide notification location for xp: none, dm, server.")
                     .ConfigureAwait(false);
                 return;
             }
@@ -75,18 +75,23 @@ namespace Roki.Modules.Xp
                 !notify.Equals("dm", StringComparison.OrdinalIgnoreCase) ||
                 !notify.Equals("server", StringComparison.OrdinalIgnoreCase))
             {
-                await ctx.Channel
-                    .EmbedAsync(new EmbedBuilder().WithErrorColor().WithDescription("Not a valid option. Options are none, dm, or server."))
+                await ctx.Channel.SendErrorAsync("Not a valid option. Options are none, dm, or server.")
                     .ConfigureAwait(false);
                 return;
             }
 
             using var uow = _db.GetDbContext();
             if (!isAdmin)
+            {
                 await uow.Users.ChangeNotificationLocation(ctx.User.Id, notify).ConfigureAwait(false);
+            }
             else
+            {
                 await uow.Users.ChangeNotificationLocation(user.Id, notify).ConfigureAwait(false);
-            await ctx.Channel.EmbedAsync(new EmbedBuilder().WithOkColor().WithDescription("Successfully changed xp notification preferences."))
+            }
+            
+            await ctx.Channel.EmbedAsync(new EmbedBuilder().WithOkColor()
+                    .WithDescription("Successfully changed xp notification preferences."))
                 .ConfigureAwait(false);
         }
     }
