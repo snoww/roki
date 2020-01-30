2ee1dc0f3f4c7898d743f774412fb58f7fd43534
Lou <lou.zhang@mail.utoronto.ca>
2020-01-30 14:13:03 -0500

    roki: use logger instead of console.writeline for fill

--- Roki.Core/Modules/Administration/Services/AdministrationService.cs
+++ Roki.Core/Modules/Administration/Services/AdministrationService.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Discord;
 using Discord.WebSocket;
+using NLog;
 using Roki.Services;
 
 namespace Roki.Modules.Administration.Services 
@@ -12,45 +13,46 @@ namespace Roki.Modules.Administration.Services
     {
         private readonly DbService _db;
         private readonly DiscordSocketClient _client;
-        private readonly Roki _roki;
+        private readonly Logger _log;
 
-        public AdministrationService(DbService db, DiscordSocketClient client, Roki roki)
+        public AdministrationService(DbService db, DiscordSocketClient client)
         {
             _db = db;
             _client = client;
-            _roki = roki;
+            _log = LogManager.GetCurrentClassLogger();
         }
 
         public async Task FillMissingMessagesAsync(ulong messageId)
         {
             using var uow = _db.GetDbContext();
-            var channels = uow.Context.Channels.Where(c => c.Logging).ToList();
+            var channels = uow.Context.Channels.Where(c => c.Logging).ToArray();
             foreach (var channel in channels.Select(ch => _client.GetChannel(ch.ChannelId) as ITextChannel))
             {
                 if (channel == null) return;
-                Console.Write($"{channel.Name}: ");
-                IEnumerable<IMessage> messages;
+                
                 try
                 {
-                    messages = await channel.GetMessagesAsync(messageId, Direction.After, int.MaxValue).FlattenAsync().ConfigureAwait(false);
+                    _log.Info("Downloading messages from {0}", channel.Name);
+                    var messages = await channel.GetMessagesAsync(messageId, Direction.After, int.MaxValue).FlattenAsync().ConfigureAwait(false);
+                    var messageList = messages.ToArray();
+                    _log.Info("{0}: {1} messages total", channel.Name, messageList.Length);
+                    if (!messageList.Any()) continue;
+                
+                    var count = 0;
+                    foreach (var message in messageList.Reverse())
+                    {
+                        if (message.Author.IsBot) continue;
+                        if (await uow.Messages.MessageExists(message.Id).ConfigureAwait(false)) continue;
+                        await uow.Messages.AddMissingMessageAsync(message).ConfigureAwait(false);
+                        count++;
+                    }
+
+                    _log.Info("{0} total messages added", count);
                 }
                 catch (Exception e)
                 {
-                    Console.WriteLine(e);
-                    continue;
+                    _log.Error(e);
                 }
-                Console.WriteLine(messages.Count());
-                if (!messages.Any()) continue;
-                
-                var count = 0;
-                foreach (var message in messages.Reverse())
-                {
-                    if (message.Author.IsBot) continue;
-                    if (await uow.Messages.MessageExists(message.Id).ConfigureAwait(false)) continue;
-                    await uow.Messages.AddMissingMessageAsync(message).ConfigureAwait(false);
-                    count++;
-                }
-                Console.WriteLine($"{count} total messages added");
             }
         }
     }
