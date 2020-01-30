5fb7048087467dbb9743e6609482c49ab13ac13f
snow <lz@snow.xyz>
2020-01-30 16:19:36 -0500

    roki: change time method to use timezoneinfo

--- Roki.Core/Modules/Searches/Models/TimeData.cs
+++ Roki.Core/Modules/Searches/Models/TimeData.cs
@@ -5,7 +5,7 @@ namespace Roki.Modules.Searches.Models
     public class TimeData
     {
         public string Address { get; set; }
-        public DateTime Time { get; set; }
+        public DateTimeOffset Time { get; set; }
         public string TimeZoneName { get; set; }
     }
 }
