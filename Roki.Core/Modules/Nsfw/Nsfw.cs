46feac86e50c1c65a14941a942e9ea552e6729f8
Lou <lou.zhang@mail.utoronto.ca>
2020-01-30 21:21:37 -0500

    nsfw: forget it... just send url of content

--- Roki.Core/Modules/Nsfw/Nsfw.cs
+++ Roki.Core/Modules/Nsfw/Nsfw.cs
@@ -12,64 +12,64 @@ namespace Roki.Modules.Nsfw
         public async Task NotSafeForWork()
         {
             await Context.Channel.TriggerTypingAsync().ConfigureAwait(false);
-            var path = await Service.GetRandomNsfw(NsfwCategory.General).ConfigureAwait(false);
-            await Context.Channel.SendFileAsync(path).ConfigureAwait(false);
+            var url = await Service.GetRandomNsfw(NsfwCategory.General).ConfigureAwait(false);
+            await Context.Channel.SendMessageAsync(url).ConfigureAwait(false);
         }
         
         [RokiCommand, Usage, Description, Aliases]
         public async Task Ass()
         {
             await Context.Channel.TriggerTypingAsync().ConfigureAwait(false);
-            var path = await Service.GetRandomNsfw(NsfwCategory.Ass).ConfigureAwait(false);
-            await Context.Channel.SendFileAsync(path).ConfigureAwait(false);
+            var url = await Service.GetRandomNsfw(NsfwCategory.Ass).ConfigureAwait(false);
+            await Context.Channel.SendMessageAsync(url).ConfigureAwait(false);
         }
         
         [RokiCommand, Usage, Description, Aliases]
         public async Task Boobs()
         {
             await Context.Channel.TriggerTypingAsync().ConfigureAwait(false);
-            var path = await Service.GetRandomNsfw(NsfwCategory.Boobs).ConfigureAwait(false);
-            await Context.Channel.SendFileAsync(path).ConfigureAwait(false);
+            var url = await Service.GetRandomNsfw(NsfwCategory.Boobs).ConfigureAwait(false);
+            await Context.Channel.SendMessageAsync(url).ConfigureAwait(false);
         }
         
         [RokiCommand, Usage, Description, Aliases]
         public async Task Cosplay()
         {
             await Context.Channel.TriggerTypingAsync().ConfigureAwait(false);
-            var path = await Service.GetRandomNsfw(NsfwCategory.Cosplay).ConfigureAwait(false);
-            await Context.Channel.SendFileAsync(path).ConfigureAwait(false);
+            var url = await Service.GetRandomNsfw(NsfwCategory.Cosplay).ConfigureAwait(false);
+            await Context.Channel.SendMessageAsync(url).ConfigureAwait(false);
         }
         
         [RokiCommand, Usage, Description, Aliases]
         public async Task Hentai()
         {
             await Context.Channel.TriggerTypingAsync().ConfigureAwait(false);
-            var path = await Service.GetRandomNsfw(NsfwCategory.Hentai).ConfigureAwait(false);
-            await Context.Channel.SendFileAsync(path).ConfigureAwait(false);
+            var url = await Service.GetRandomNsfw(NsfwCategory.Hentai).ConfigureAwait(false);
+            await Context.Channel.SendMessageAsync(url).ConfigureAwait(false);
         }
         
         [RokiCommand, Usage, Description, Aliases]
         public async Task Mild()
         {
             await Context.Channel.TriggerTypingAsync().ConfigureAwait(false);
-            var path = await Service.GetRandomNsfw(NsfwCategory.Mild).ConfigureAwait(false);
-            await Context.Channel.SendFileAsync(path).ConfigureAwait(false);
+            var url = await Service.GetRandomNsfw(NsfwCategory.Mild).ConfigureAwait(false);
+            await Context.Channel.SendMessageAsync(url).ConfigureAwait(false);
         }
         
         [RokiCommand, Usage, Description, Aliases]
         public async Task NsfwGifs()
         {
             await Context.Channel.TriggerTypingAsync().ConfigureAwait(false);
-            var path = await Service.GetRandomNsfw(NsfwCategory.Gifs).ConfigureAwait(false);
-            await Context.Channel.SendFileAsync(path).ConfigureAwait(false);
+            var url = await Service.GetRandomNsfw(NsfwCategory.Gifs).ConfigureAwait(false);
+            await Context.Channel.SendMessageAsync(url).ConfigureAwait(false);
         }
         
         [RokiCommand, Usage, Description, Aliases]
         public async Task Trans()
         {
             await Context.Channel.TriggerTypingAsync().ConfigureAwait(false);
-            var path = await Service.GetRandomNsfw(NsfwCategory.Trans).ConfigureAwait(false);
-            await Context.Channel.SendFileAsync(path).ConfigureAwait(false);
+            var url = await Service.GetRandomNsfw(NsfwCategory.Trans).ConfigureAwait(false);
+            await Context.Channel.SendMessageAsync(url).ConfigureAwait(false);
         }
     }
 }
