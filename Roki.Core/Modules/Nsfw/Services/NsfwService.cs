4947d80dae1f8ac44c60a8a85c2c30194eec54fc
snow <lz@snow.xyz>
2020-01-30 21:21:37 -0500

    nsfw: forget it... just send url of content

--- Roki.Core/Modules/Nsfw/Services/NsfwService.cs
+++ Roki.Core/Modules/Nsfw/Services/NsfwService.cs
@@ -37,38 +37,7 @@ namespace Roki.Modules.Nsfw.Services
                 var result = await http.GetStringAsync(string.Format(BaseUrl, options[rng])).ConfigureAwait(false);
                 using var json = JsonDocument.Parse(result);
                 // maybe return source as well?
-                var url = json.RootElement[0].GetProperty("data").GetProperty("children")[0].GetProperty("data").GetProperty("url").GetString();
-                string path;
-
-                if (url.Contains("gfycat", StringComparison.OrdinalIgnoreCase))
-                {
-                    var gfycat = await http.GetStringAsync(url).ConfigureAwait(false);
-                    const string search = "\"mp4Url\":\"";
-                    var index = gfycat.IndexOf(search, StringComparison.Ordinal);
-                    var gfycatPath = new StringBuilder();
-                    for (int i = index + search.Length; i < gfycat.Length; i++)
-                    {
-                        if (gfycat[i] == '4')
-                        {
-                            gfycatPath.Append(4);
-                            break;
-                        }
-
-                        gfycatPath.Append(gfycat[i]);
-                    }
-
-                    url = gfycatPath.ToString().Replace("\\u002F", "/");
-                    path = $"./temp/{Path.GetFileName(url)}";
-                }
-                else
-                {
-                    path = $"./temp/{Path.GetFileName(url)}";
-                }
-                
-                using var client = new WebClient();
-                await client.DownloadFileTaskAsync(url, path).ConfigureAwait(false);
-                
-                return path;
+                return json.RootElement[0].GetProperty("data").GetProperty("children")[0].GetProperty("data").GetProperty("url").GetString();
             }
             catch (Exception e)
             {
