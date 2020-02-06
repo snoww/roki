cb3b9a05e3d728cb49874b7bc92a3d352a2e95b4
snow <lz@snow.xyz>
2020-02-06 15:15:58 -0500

    db: added nullables to transactions

--- Roki.Core/Services/Database/Core/CurrencyTransaction.cs
+++ Roki.Core/Services/Database/Core/CurrencyTransaction.cs
@@ -11,10 +11,10 @@ namespace Roki.Services.Database.Core
         public int Id { get; set; }
         public long Amount { get; set; }
         public string Reason { get; set; }
-        public ulong To { get; set; }
-        public ulong From { get; set; }
-        public ulong GuildId { get; set; }
-        public ulong ChannelId { get; set; }
+        public ulong? To { get; set; }
+        public ulong? From { get; set; }
+        public ulong? GuildId { get; set; }
+        public ulong? ChannelId { get; set; }
         public ulong MessageId { get; set; }
         public DateTimeOffset TransactionDate { get; set; } = DateTimeOffset.UtcNow;
     }
