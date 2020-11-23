a6a644a0d77540a4e5839341febdd766fe9bf3ac
Lou <lou.zhang@mail.utoronto.ca>
2020-11-23 14:14:30 -0500

    roki: null values are no longer inserted in db document

--- Roki.Core/Services/Database/Maps/Message.cs
+++ Roki.Core/Services/Database/Maps/Message.cs
@@ -13,8 +13,11 @@ namespace Roki.Services.Database.Maps
         public ulong ChannelId { get; set; }
         public ulong? GuildId { get; set; }
         public string Content { get; set; }
+        [BsonIgnoreIfNull]
         public ulong? MessageReference { get; set; }
+        [BsonIgnoreIfNull]
         public List<Edit> Edits { get; set; } = new List<Edit>();
+        [BsonIgnoreIfNull]
         public List<string> Attachments { get; set; } = new List<string>();
         public DateTime Timestamp { get; set; }
         public bool IsDeleted { get; set; }
