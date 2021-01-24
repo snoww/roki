5d055405833e3e7e2f4c00ac9b996e1d100c708b
Lou <lou.zhang@mail.utoronto.ca>
2021-01-24 17:55:06 -0500

    games: reworked pokemon showdown module

---
+++ Roki/scripts/showdown.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+/home/snow/Documents/showdown/run.py /home/snow/Documents/showdown/roki1.env &
+sleep 2
+/home/snow/Documents/showdown/run.py /home/snow/Documents/showdown/roki.env &
